8283f3f6850ee6c9251f28a2c89b07e1d8d3e47f
Pejmanfsd <pejmanfsd@gmail.com>
2025-10-23 18:51:41 -0400

    User can choose
    The user can choose one of the three options.

--- src/RokScissorsPaper/RokScissorsPaper.jsx
+++ src/RokScissorsPaper/RokScissorsPaper.jsx
@@ -1,13 +1,19 @@
+import {useState} from 'react';
 import Rock from './Rok.png';
 import Scissors from './Scissors.png';
 import Paper from './Paper.png';
 
 export default function RokScissorsPaper({updateTotalPoint, totalPoint}) {
+    const [userChoice, setUserChoice] = useState("");
+    const handleUserChoice = (input) => {
+        setUserChoice(input);
+    }
     return (
         <div>
-            <img src={Rock} width='150px' alt='Rock' />
-            <img src={Scissors} width='150px' alt='Scissors' />
-            <img src={Paper} width='150px' alt='Paper' />
+            <img src={Rock} width='150px' alt='Rock' onClick={() => handleUserChoice('Rock')} />
+            <img src={Scissors} width='150px' alt='Scissors' onClick={() => handleUserChoice('Scissors')} />
+            <img src={Paper} width='150px' alt='Paper' onClick={() => handleUserChoice('Paper')} />
+            {userChoice}
         </div>
     )
 }
