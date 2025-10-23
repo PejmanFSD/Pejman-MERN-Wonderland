8ccc76e1569bd3033adb9a6f59a1f0d9bf70fa1a
Pejmanfsd <pejmanfsd@gmail.com>
2025-10-23 18:45:25 -0400

    Import images
    Import images of Rock, Scissors and Paper

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
+import {useState} from 'react';
 import RokScissorsPaper from './RokScissorsPaper/RokScissorsPaper';
 
 function App() {
+  const [totalPoint, setTotalPoint] = useState(0);
+  const updateTotalPoint = (score) => {
+    setTotalPoint(currTotalPoint => 
+      currTotalPoint + score
+    )
+  }
   return (
     <div className="App">
       Pejman MERN Wonderland
-      <RokScissorsPaper />
+      <RokScissorsPaper updateTotalPoint={updateTotalPoint} totalPoint={totalPoint} />
     </div>
   );
 }
--- src/RokScissorsPaper/RokScissorsPaper.jsx
+++ src/RokScissorsPaper/RokScissorsPaper.jsx
@@ -1,7 +1,13 @@
+import Rock from './Rok.png';
+import Scissors from './Scissors.png';
+import Paper from './Paper.png';
+
 export default function RokScissorsPaper({updateTotalPoint, totalPoint}) {
     return (
         <div>
-            The "Rok, Scissors, Paper" project
+            <img src={Rock} width='150px' alt='Rock' />
+            <img src={Scissors} width='150px' alt='Scissors' />
+            <img src={Paper} width='150px' alt='Paper' />
         </div>
     )
 }
