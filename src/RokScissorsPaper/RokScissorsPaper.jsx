ef69f7a2c4124a5983e16a13f1a6717c865e2c04
Pejmanfsd <pejmanfsd@gmail.com>
2025-10-23 19:00:40 -0400

    Pejman can choose
    Pejman can choose one of the three options.

--- src/RokScissorsPaper/RokScissorsPaper.jsx
+++ src/RokScissorsPaper/RokScissorsPaper.jsx
@@ -2,18 +2,27 @@ import {useState} from 'react';
 import Rock from './Rok.png';
 import Scissors from './Scissors.png';
 import Paper from './Paper.png';
+import { getRand } from "../utils";
 
 export default function RokScissorsPaper({updateTotalPoint, totalPoint}) {
     const [userChoice, setUserChoice] = useState("");
+    const [pejmanChoice, setPejmanChoice] = useState("");
+    const PejmanChoice = () => {
+        const randomNum = getRand(3);
+        if (randomNum === 1) return 'Rock';
+        if (randomNum === 2) return 'Scissors';
+        if (randomNum === 3) return 'Paper';
+    }
     const handleUserChoice = (input) => {
         setUserChoice(input);
+        setPejmanChoice(PejmanChoice());
     }
     return (
         <div>
             <img src={Rock} width='150px' alt='Rock' onClick={() => handleUserChoice('Rock')} />
             <img src={Scissors} width='150px' alt='Scissors' onClick={() => handleUserChoice('Scissors')} />
             <img src={Paper} width='150px' alt='Paper' onClick={() => handleUserChoice('Paper')} />
-            {userChoice}
+            {userChoice} - {pejmanChoice}
         </div>
     )
 }
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+function getRand(maxNum) {
+    return Math.floor(Math.random() * maxNum) + 1;
+}
+
+export { getRand };
