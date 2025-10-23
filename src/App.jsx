c743291f453bcbd9ead0994c90692f61083160fb
Pejmanfsd <pejmanfsd@gmail.com>
2025-10-23 18:17:52 -0400

    Starting "Rok, Scissors, Paper" project
    The "Rok, Scissors, Paper" project begins!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import RokScissorsPaper from './RokScissorsPaper/RokScissorsPaper';
 
 function App() {
   return (
     <div className="App">
       Pejman MERN Wonderland
+      <RokScissorsPaper />
     </div>
   );
 }
---
+++ src/RokScissorsPaper/RokScissorsPaper.jsx
@@ -0,0 +1,7 @@
+export default function RokScissorsPaper({updateTotalPoint, totalPoint}) {
+    return (
+        <div>
+            The "Rok, Scissors, Paper" project
+        </div>
+    )
+}
