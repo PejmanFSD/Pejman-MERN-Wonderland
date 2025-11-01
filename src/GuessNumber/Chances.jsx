c751bfd84533dcbf63b7225eaef5b18d44c44dad
PejmanFSD <pejmanfsd@gmail.com>
2025-11-01 18:54:54 -0400

    Dynamic messages for the number of chances
    Based on the number of left chances, the message of informing the user of the number of chances they have is different.

--- src/GuessNumber/Chances.jsx
+++ src/GuessNumber/Chances.jsx
@@ -1,5 +1,24 @@
-export default function chances({chancesNum}) {
+export default function chances({ chancesNum }) {
   return (
-    <div style={{ color: "blue" }}>{`You have ${chancesNum} chances left`}</div>
+    <div>
+      {chancesNum === 10 && (
+        <div style={{ color: "blue" }}>
+          You have 10 chances to find the number
+        </div>
+      )}
+      {chancesNum > 1 && chancesNum < 10 && (
+        <div
+          style={{ color: "blue" }}
+        >{`You have ${chancesNum} chances left`}</div>
+      )}
+      {chancesNum === 1 && (
+        <div style={{ color: "red" }}>
+          Warning! You only have one chane left!
+        </div>
+      )}
+      {chancesNum === 0 && (
+        <div style={{ color: "gray" }}>Sorry! You loose!</div>
+      )}
+    </div>
   );
 }
