ae656d11781dc7ccf4357d69b632d1d36bda85d7
PejmanFSD <pejmanfsd@gmail.com>
2025-11-07 14:17:21 -0500

    Generating random countries
    Random countries can be generated by a button.

--- src/Capitals/Capitals.jsx
+++ src/Capitals/Capitals.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import countries from "./countries";
 
 export default function Capitals() {
+  const [allCountries, setAllCountries] = useState(countries);
+  const [chosenCountries, setChosenCountries] = useState([]);
+  const generateCountries = () => {
+    for (let i = 0; i < 20; i++) {
+      const randomIndex = Math.floor(Math.random() * allCountries.length);
+      setChosenCountries((currChosenCountries) => [
+        ...currChosenCountries,
+        allCountries[randomIndex].country,
+      ]);
+    }
+  };
   return (
-    <ul>
-      {countries.map((c, index) => (
-        <li key={index}>
-          {c.country} — {c.capital}
-        </li>
+    <div>
+      {chosenCountries.map((i) => (
+        <div>{i}</div>
       ))}
-    </ul>
+      <button onClick={() => generateCountries()}>Generate Countries</button>
+    </div>
   );
 }
