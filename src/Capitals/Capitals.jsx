e30a553d10ef67f09862ff27de4fa1a6530d4244
PejmanFSD <pejmanfsd@gmail.com>
2025-11-08 01:48:20 -0500

    Generating 20 random countries with their capitals.
    By clicking on the "Start" button, the user can create and render 20 random countries with their capitals.

--- src/Capitals/Capitals.jsx
+++ src/Capitals/Capitals.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import countries from "./countries";
-const indexesArray = new Array(countries.length).fill(null).map((c, i) => i);
+// const countryNames = countries.map((c) => c.country);
+// const capitalNames = countries.map((c) => c.capital);
 export default function Capitals() {
-  const [indexes, setIndexes] = useState(indexesArray);
+  const [pack, setPack] = useState(countries);
+  const [questionCountries, setQuestionCountries] = useState([]);
+  const [questionCapitals, setQuestionCapitals] = useState([]);
   const shuffleArray = (array) => {
     const arr = [...array];
     for (let i = arr.length - 1; i > 0; i--) {
@@ -11,13 +14,30 @@ export default function Capitals() {
     }
     return arr;
   };
-  const handleShuffle = () => {
-    setIndexes((prev) => shuffleArray(prev));
+  const handleStart = () => {
+    setPack((prev) => shuffleArray(prev));
+    setQuestionCountries(pack.map((c) => c.country));
+    setQuestionCapitals(pack.map((c) => c.capital));
   };
   return (
     <div>
-      <p>Indexes: {indexes.join(", ")}</p>
-      <button onClick={() => handleShuffle()}>Shuffle</button>
+      <button onClick={() => handleStart()}>Start</button>
+      <div>
+        <h3>Countries</h3>
+        {questionCountries.slice(1, 21).map((qc, i) => (
+          <div>
+            {i + 1} - {qc}
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>Capitals</h3>
+        {questionCapitals.slice(1, 21).map((qc, i) => (
+          <div>
+            {i + 1} - {qc}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
