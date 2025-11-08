3061265c053959f8990ababc673bb2f9941bdac0
PejmanFSD <pejmanfsd@gmail.com>
2025-11-08 12:49:11 -0500

    Optimizing the process of generating random countries.
    Now if the user clicks on the "start" button, 30 random countries and their capitals show up in UI.

--- src/Capitals/Capitals.jsx
+++ src/Capitals/Capitals.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import countries from "./countries";
-// const countryNames = countries.map((c) => c.country);
-// const capitalNames = countries.map((c) => c.capital);
+const countryNames = countries.map((c) => c.country);
+const capitalNames = countries.map((c) => c.capital);
 export default function Capitals() {
   const [pack, setPack] = useState(countries);
-  const [questionCountries, setQuestionCountries] = useState([]);
-  const [questionCapitals, setQuestionCapitals] = useState([]);
+  const [questionCountries, setQuestionCountries] = useState(countryNames);
+  const [questionCapitals, setQuestionCapitals] = useState(capitalNames);
+  const [showQuestion, setShowQuestion] = useState(false);
   const shuffleArray = (array) => {
     const arr = [...array];
     for (let i = arr.length - 1; i > 0; i--) {
@@ -16,28 +17,40 @@ export default function Capitals() {
   };
   const handleStart = () => {
     setPack((prev) => shuffleArray(prev));
-    setQuestionCountries(pack.map((c) => c.country));
-    setQuestionCapitals(pack.map((c) => c.capital));
+    setQuestionCountries(pack.map((c) => c.country).slice(1, 31));
+    setQuestionCapitals(pack.map((c) => c.capital).slice(1, 31));
+    setShowQuestion(true);
   };
+  useEffect(function () {
+    setPack((prev) => shuffleArray(prev));
+  }, []);
   return (
     <div>
       <button onClick={() => handleStart()}>Start</button>
-      <div>
-        <h3>Countries</h3>
-        {questionCountries.slice(1, 21).map((qc, i) => (
+      {showQuestion && (
+        <div>
           <div>
-            {i + 1} - {qc}
+            <h3>Countries</h3>
+            <div>
+              {questionCountries.map((qc, i) => (
+                <div>
+                  {i + 1} - {qc}
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
-      <div>
-        <h3>Capitals</h3>
-        {questionCapitals.slice(1, 21).map((qc, i) => (
           <div>
-            {i + 1} - {qc}
+            <h3>Capitals</h3>
+            <div>
+              {questionCapitals.map((qc, i) => (
+                <div>
+                  {i + 1} - {qc}
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
