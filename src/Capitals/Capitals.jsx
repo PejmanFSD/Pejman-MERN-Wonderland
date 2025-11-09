c8b41495490c5ff8ba5d69cba97fd459366d0014
PejmanFSD <pejmanfsd@gmail.com>
2025-11-09 14:01:55 -0500

    Making the values of the <input> tags dynamic.
    The values of the <input> tags get updated after each change.

--- src/Capitals/Capitals.jsx
+++ src/Capitals/Capitals.jsx
@@ -2,11 +2,23 @@ import { useState, useEffect } from "react";
 import countries from "./countries";
 const countryNames = countries.map((c) => c.country);
 const capitalNames = countries.map((c) => c.capital);
+
 export default function Capitals() {
   const [pack, setPack] = useState(countries);
   const [questionCountries, setQuestionCountries] = useState(countryNames);
   const [questionCapitals, setQuestionCapitals] = useState(capitalNames);
-  const [inputs, setInputs] = useState([]);
+  const [inputs, setInputs] = useState({
+    input1: "",
+    input2: "",
+    input3: "",
+    input4: "",
+    input5: "",
+    input6: "",
+    input7: "",
+    input8: "",
+    input9: "",
+    input10: "",
+  });
   const [showCountries, setShowCountries] = useState(false);
   const [showCapitals, setShowCapitals] = useState(false);
   const handleChange = (e) => {
@@ -16,7 +28,7 @@ export default function Capitals() {
     const { name, value } = e.target;
     setInputs((currInputs) => {
       currInputs[name] = value;
-      return [...currInputs, value];
+      return { ...currInputs };
     });
   };
   const shuffleArray = (array) => {
@@ -29,8 +41,8 @@ export default function Capitals() {
   };
   const handleStart = () => {
     setPack((currPack) => shuffleArray(currPack));
-    setQuestionCountries(pack.map((c) => c.country).slice(1, 31));
-    setQuestionCapitals(pack.map((c) => c.capital).slice(1, 31));
+    setQuestionCountries(pack.map((c) => c.country).slice(1, 11));
+    setQuestionCapitals(pack.map((c) => c.capital).slice(1, 11));
     setShowCountries(true);
   };
   const handleShowCapitals = () => {
@@ -46,8 +58,10 @@ export default function Capitals() {
     <div>
       <button onClick={() => handleStart()}>Start</button>
       <h3>inputs:</h3>
-      {inputs.map((i) => (
-        <div>{i}</div>
+      {Object.values(inputs).map((value) => (
+        <p key={value} style={{ display: "inline" }}>
+          {value}
+        </p>
       ))}
       {showCountries && (
         <div>
@@ -62,7 +76,7 @@ export default function Capitals() {
       {!showCapitals && showCountries && (
         <div>
           <h4>
-            30 countries are chosen for you, guess their capitals correctly and
+            10 countries are chosen for you, guess their capitals correctly and
             win the game
           </h4>
           <button onClick={() => handleShowCapitals()}>Ok</button>
@@ -81,18 +95,106 @@ export default function Capitals() {
       {showCapitals && (
         <div>
           <form>
-            {questionCountries.map((el) => (
-              <div>
-                <label htmlFor={`${el}`}></label>
-                <input
-                  type="number"
-                  placeholder={`Capital of ${el}`}
-                  name={`${el}`}
-                  id={`${el}`}
-                  onChange={handleChange}
-                />
-              </div>
-            ))}
+            <label htmlFor="input1"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[0]}`}
+              name="input1"
+              id="input1"
+              value={inputs.input1}
+              onChange={handleChange}
+            />
+            <label htmlFor="input2"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[1]}`}
+              name="input2"
+              id="input2"
+              value={inputs.input2}
+              onChange={handleChange}
+            />
+            <label htmlFor="input3"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[2]}`}
+              name="input3"
+              id="input3"
+              value={inputs.input3}
+              onChange={handleChange}
+            />
+            <label htmlFor="input4"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[3]}`}
+              name="input4"
+              id="input4"
+              value={inputs.input4}
+              onChange={handleChange}
+            />
+            <label htmlFor="input5"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[4]}`}
+              name="input5"
+              id="input5"
+              value={inputs.input5}
+              onChange={handleChange}
+            />
+            <label htmlFor="input6"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[5]}`}
+              name="input6"
+              id="input6"
+              value={inputs.input6}
+              onChange={handleChange}
+            />
+            <label htmlFor="input7"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[6]}`}
+              name="input7"
+              id="input7"
+              value={inputs.input7}
+              onChange={handleChange}
+            />
+            <label htmlFor="input8"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[7]}`}
+              name="input8"
+              id="input8"
+              value={inputs.input8}
+              onChange={handleChange}
+            />
+            <label htmlFor="input9"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[8]}`}
+              name="input9"
+              id="input9"
+              value={inputs.input9}
+              onChange={handleChange}
+            />
+            <label htmlFor="input10"></label>
+            <input
+              style={{ display: "block" }}
+              type="text"
+              placeholder={`Capital of ${questionCountries[9]}`}
+              name="input10"
+              id="input10"
+              value={inputs.input10}
+              onChange={handleChange}
+            />
           </form>
         </div>
       )}
