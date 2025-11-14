e96f1fb590200c945f3d831c956a214355ea6ccc
PejmanFSD <pejmanfsd@gmail.com>
2025-11-14 11:59:43 -0500

    Fetching an advice from the external API.
    For the beginning, a random advice is fetched from the external API.

--- src/Cryptogram/Cryptogram.jsx
+++ src/Cryptogram/Cryptogram.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function Cryptogram() {
-    return (
-        <div>
-            Cryptogram
-        </div>
-    )
+  const [advice, setAdvice] = useState("");
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+  return (
+    <div>
+      <h3>{advice}</h3>
+      <button onClick={getAdvice}>Get Advice</button>
+    </div>
+  );
 }
