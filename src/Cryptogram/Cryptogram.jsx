5d1fce55a6e98aab2385ec491eb8fbe8ffe11ccf
PejmanFSD <pejmanfsd@gmail.com>
2025-11-14 12:55:07 -0500

    Presenting the advice as an array.
    Each fetched advice is transformed into an array, the array is displayed on UI.

--- src/Cryptogram/Cryptogram.jsx
+++ src/Cryptogram/Cryptogram.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 
 export default function Cryptogram() {
-  const [advice, setAdvice] = useState("");
+  //   const [advice, setAdvice] = useState("");
+  const [adviceArray, setAdviceArray] = useState([]);
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    setAdvice(data.slip.advice);
+    // setAdvice(data.slip.advice);
+    convertStringIntoArray(data.slip.advice);
   }
+  const convertStringIntoArray = (str) => {
+    for (let i = 0; i < str.length; i++) {
+      setAdviceArray((currAdviceArray) => [...currAdviceArray, str[i]]);
+    }
+  };
   return (
     <div>
-      <h3>{advice}</h3>
+      <div>
+        {adviceArray.map((a) => (
+          <h2 style={{ display: "inline" }}>{a}</h2>
+        ))}
+      </div>
       <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
