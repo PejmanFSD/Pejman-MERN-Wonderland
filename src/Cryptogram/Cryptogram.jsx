beae44703c9a02f1bb01c22f0e191f0e8d641cfd
PejmanFSD <pejmanfsd@gmail.com>
2025-11-15 12:29:17 -0500

    Generating the number of repetition of each letter.
    We can see the number that each letter is repeated in the advice.

--- src/Cryptogram/Cryptogram.jsx
+++ src/Cryptogram/Cryptogram.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Cryptogram() {
-  //   const [advice, setAdvice] = useState("");
   const [adviceArray, setAdviceArray] = useState([]);
+  const [resultObj, setResultObj] = useState({
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0,
+  });
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
-    // setAdvice(data.slip.advice);
     convertStringIntoArray(data.slip.advice);
   }
   const convertStringIntoArray = (str) => {
@@ -14,11 +40,36 @@ export default function Cryptogram() {
       setAdviceArray((currAdviceArray) => [...currAdviceArray, str[i]]);
     }
   };
+  useEffect(
+    function () {
+      function generateResult() {
+        const copy = adviceArray.slice().map((item) => item.toLowerCase());
+        setResultObj((currResultObj) => {
+          const updated = { ...currResultObj };
+          copy.forEach((item) => {
+            if (item in updated) {
+              updated[item] += 1;
+            }
+          });
+          return updated;
+        });
+      }
+      generateResult();
+    },
+    [adviceArray]
+  );
   return (
     <div>
       <div>
         {adviceArray.map((a) => (
           <h2 style={{ display: "inline" }}>{a}</h2>
+        ))}
+      </div>
+      <div>
+        {Object.entries(resultObj).map(([key, value]) => (
+          <div key={key}>
+            {key}: {value}
+          </div>
         ))}
       </div>
       <button onClick={getAdvice}>Get Advice</button>
