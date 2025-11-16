75d2dc7aff032c8706d4ecc4345daa1b03cff38d
PejmanFSD <pejmanfsd@gmail.com>
2025-11-15 21:08:33 -0500

    Replaces the first four most repeated letters with an <input> tag.
    Each of the first four most repeated letters is now an <input> tag. All these <input> tags are in one <form>/

--- src/Cryptogram/Cryptogram.jsx
+++ src/Cryptogram/Cryptogram.jsx
@@ -71,15 +71,71 @@ export default function Cryptogram() {
         ))}
       </div>
       <form>
-        {adviceArray.map((a) => (
-            Object.keys(resultObj).slice(0, 4).includes(a.toLowerCase()) ?
-            (Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) === 0 ? (<h2 style={{ display: "inline", color: "red" }}>1</h2>) :
-            (Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) === 1 ? (<h2 style={{ display: "inline", color: "red" }}>2</h2>) :
-            (Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) === 2 ? (<h2 style={{ display: "inline", color: "red" }}>3</h2>) :
-            (Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) === 3 && (<h2 style={{ display: "inline", color: "red" }}>4</h2>)
-          )))) :
-          <h2 style={{ display: "inline" }}>{a}</h2>
-        ))}
+        {adviceArray.map((a) =>
+          Object.keys(resultObj).slice(0, 4).includes(a.toLowerCase()) ? (
+            Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) ===
+            0 ? (
+              <div style={{ display: "inline" }}>
+                <label htmlFor="input1"></label>
+                <input
+                  type="text"
+                  placeholder="1"
+                  name="input1"
+                  id="input1"
+                  // value={inputs.input1}
+                  // onChange={handleChange}
+                  style={{ width: "15px", height: "30px" }}
+                />
+              </div>
+            ) : Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) ===
+              1 ? (
+              <div style={{ display: "inline" }}>
+                <label htmlFor="input2"></label>
+                <input
+                  type="text"
+                  placeholder="2"
+                  name="input2"
+                  id="input2"
+                  // value={inputs.input2}
+                  // onChange={handleChange}
+                  style={{ width: "15px", height: "30px" }}
+                />
+              </div>
+            ) : Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) ===
+              2 ? (
+              <div style={{ display: "inline" }}>
+                <label htmlFor="input3"></label>
+                <input
+                  type="text"
+                  placeholder="3"
+                  name="input3"
+                  id="input3"
+                  // value={inputs.input3}
+                  // onChange={handleChange}
+                  style={{ width: "15px", height: "30px" }}
+                />
+              </div>
+            ) : (
+              Object.keys(resultObj).slice(0, 4).indexOf(a.toLowerCase()) ===
+                3 && (
+                <div style={{ display: "inline" }}>
+                  <label htmlFor="input4"></label>
+                  <input
+                    type="text"
+                    placeholder="4"
+                    name="input4"
+                    id="input4"
+                    // value={inputs.input4}
+                    // onChange={handleChange}
+                    style={{ width: "15px", height: "30px" }}
+                  />
+                </div>
+              )
+            )
+          ) : (
+            <h2 style={{ display: "inline" }}>{a}</h2>
+          )
+        )}
       </form>
       <div>
         {Object.entries(resultObj).map(([key, value]) => (
