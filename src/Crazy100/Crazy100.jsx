bc6917642dee99a6c41e58d67dbf03b29a6b100f
PejmanFSD <pejmanfsd@gmail.com>
2025-11-18 22:22:11 -0500

    Generating 4 random integers whose total is 100.
    A <button> is created to generate 4 random integers whose total is 100.

--- src/Crazy100/Crazy100.jsx
+++ src/Crazy100/Crazy100.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import {getRandNumInRange} from "../utils";
+
 export default function Crazy100() {
-    return (
-        <div>
-            Crazy100!
-        </div>
-    )
+  const [nums, setNums] = useState([]);
+  const generateNums = () => {
+    const num1 = getRandNumInRange(5, 33);
+    const num2 = getRandNumInRange(5, 33);
+    const num3 = getRandNumInRange(5, 33);
+    setNums((currNums) => [...currNums, num1]);
+    setNums((currNums) => [...currNums, num2]);
+    setNums((currNums) => [...currNums, num3]);
+    setNums((currNums) => [...currNums, 100 - num1 - num2 - num3]);
+  };
+  return (
+    <div>
+      {nums.map((n) => (
+        <div>{n}</div>
+      ))}
+      <button onClick={() => generateNums()}>Generate Numbers</button>
+    </div>
+  );
 }
--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,12 @@ function getRandNum(maxNum) {
     return Math.floor(Math.random() * maxNum) + 1;
 }
 
+function getRandNumInRange(minNum, maxNum) {
+    return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+  }
+
 function getRandArr(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export { getRandNum, getRandArr };
+export { getRandNum, getRandNumInRange, getRandArr };
