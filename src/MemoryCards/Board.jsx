bcf8756a7cb185b71c0bb3ca737b7c643fea6ffe
PejmanFSD <pejmanfsd@gmail.com>
2025-11-23 15:13:44 -0500

    Showing cards that are in the "visibleCards" array.
    The applications has a condition that shows only the cards in the "visibleCards" array, which is an array that contains the cards that has been clicked.

--- src/MemoryCards/Board.jsx
+++ src/MemoryCards/Board.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import Card from "./Card";
+import Emoji1 from "./emoji1.jpg";
 import Hide from "./hide.jpg";
 
 export default function Board({ nrows, ncols }) {
   const [board, setBoard] = useState([]);
-  // const [visibleCards, setVisibleCards] = useState([]);
+  const [visibleCards, setVisibleCards] = useState([
+    // [4, 4],
+    // [5, 6],
+  ]);
   const generateBoard = () => {
     for (let b = 0; b < nrows; b++) {
       let row = [];
       for (let a = 0; a < ncols; a++) {
-        row.push([a, b, <Card image={Hide} />]);
+        row.push([a, b]);
+        console.log(row);
       }
       setBoard((currBoard) => [...currBoard, row]);
     }
@@ -18,11 +23,22 @@ export default function Board({ nrows, ncols }) {
     <div>
       {board.map((row) =>
         row.map((card) =>
-          card[0] !== ncols - 1 ? (
-            <div style={{ display: "inline" }}>{card[2]}</div>
+          visibleCards.some(
+            (pair) => pair[0] === card[0] && pair[1] === card[1]
+          ) ? (
+            card[0] !== ncols - 1 ? (
+              <div style={{ display: "inline" }}>{<Card image={Emoji1} />}</div>
+            ) : (
+              <div style={{ display: "inline" }}>
+                {<Card image={Emoji1} />}
+                <br></br>
+              </div>
+            )
+          ) : card[0] !== ncols - 1 ? (
+            <div style={{ display: "inline" }}>{<Card image={Hide} />}</div>
           ) : (
             <div style={{ display: "inline" }}>
-              {card[2]}
+              {<Card image={Hide} />}
               <br></br>
             </div>
           )
