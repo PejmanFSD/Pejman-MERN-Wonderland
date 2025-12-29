c09024d74c8fe894221b89d604fa576722756d81
Pejmanfsd <pejmanfsd@gmail.com>
2025-12-29 15:02:54 -0500

    Creating a great data store for the chosen word.
    A separate file is created with lots of words, much better than the "faker" library.

--- src/HappyFlower/Form.jsx
+++ src/HappyFlower/Form.jsx
@@ -1,47 +1,49 @@
-import { faker } from "@faker-js/faker";
-const titles = ['Animal', 'Color', 'Car', 'Job', 'Book', 'Country', 'City'];
+import { animals, colors, cars, jobs, books, countries, cities } from "./words";
+import { getRandArr } from "../utils";
+const titles = ["Animal", "Color", "Car", "Job", "Book", "Country", "City"];
 
-export default function Form({ title, setTitle, word, setWord, setWordWithNoSpace, setIsGameStarted }) {
-    const handleChange = (e) => {
-        setTitle(e.target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (title === titles[0]) {
-            setWord(faker.animal.type());
-        }
-        else if (title === titles[1]) {
-            setWord(faker.color.human());
-        }
-        else if (title === titles[2]) {
-            setWord(faker.vehicle.vehicle());
-        }
-        else if (title === titles[3]) {
-            setWord(faker.person.jobTitle());
-        }
-        else if (title === titles[4]) {
-            setWord(faker.book.title());
-        }
-        else if (title === titles[5]) {
-            setWord(faker.location.country());
-        }
-        else if (title === titles[6]) {
-            setWord(faker.location.city());
-        }
-        setIsGameStarted(true);
+export default function Form({
+  title,
+  setTitle,
+  word,
+  setWord,
+  setWordWithNoSpace,
+  setIsGameStarted,
+}) {
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title === titles[0]) {
+      setWord(getRandArr(animals));
+    } else if (title === titles[1]) {
+      setWord(getRandArr(colors));
+    } else if (title === titles[2]) {
+      setWord(getRandArr(cars));
+    } else if (title === titles[3]) {
+      setWord(getRandArr(jobs));
+    } else if (title === titles[4]) {
+      setWord(getRandArr(books));
+    } else if (title === titles[5]) {
+      setWord(getRandArr(countries));
+    } else if (title === titles[6]) {
+      setWord(getRandArr(cities));
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="titles">Select one of the titles</label>
-            <select
-                onChange={handleChange}
-            >
-                <option disabled selected>⬇️</option>
-                {titles.map((t) => (
-                    <option>{t}</option>
-                ))}
-            </select>
-            {title && <button>Done</button>}
-        </form>
-    );
+    setIsGameStarted(true);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="titles">Select one of the titles</label>
+      <select onChange={handleChange}>
+        <option disabled selected>
+          ⬇️
+        </option>
+        {titles.map((t) => (
+          <option>{t}</option>
+        ))}
+      </select>
+      {title && <button>Done</button>}
+    </form>
+  );
 }
