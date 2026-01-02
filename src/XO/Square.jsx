17b87c7f285e5fbc26bc04a46b3df20451b5f30d
Pejmanfsd <pejmanfsd@gmail.com>
2026-01-02 16:24:42 -0500

    The user can click on any square and change it.
    The user can change the square to their sign ("X" or "O") by clicking on it.

--- src/XO/Square.jsx
+++ src/XO/Square.jsx
@@ -1,5 +1,22 @@
-export default function Square({imgSrc, idx}) {
-    return (
-        <img src={imgSrc} width="50px" alt="Square" style={{display: "inline"}} />
-    )
+export default function Square({ imgSrc, idx, signs, userSign, setSquares }) {
+  const handleClickSquare = () => {
+    if (userSign === "X") {
+      setSquares((currSquares) =>
+        currSquares.map((s) => (s.id === idx ? { ...s, imgSrc: signs[1] } : s))
+      );
+    } else if (userSign === "O") {
+      setSquares((currSquares) =>
+        currSquares.map((s) => (s.id === idx ? { ...s, imgSrc: signs[2] } : s))
+      );
+    }
+  };
+  return (
+    <img
+      src={imgSrc}
+      width="50px"
+      alt="Square"
+      style={{ display: "inline" }}
+      onClick={handleClickSquare}
+    />
+  );
 }
--- src/XO/XO.jsx
+++ src/XO/XO.jsx
@@ -3,27 +3,29 @@ import Square from "./Square";
 import S from "./Images/S.jpg";
 import X from "./Images/X.jpg";
 import O from "./Images/O.jpg";
+const signs = [S, X, O];
 
 export default function XO() {
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [userSign, setUserSign] = useState("");
+//   const [imgSrc, setImgSrc] = useState(signs[0]);
   const [squares, setSquares] = useState([
-    { id: 0, owner: "" },
-    { id: 1, owner: "" },
-    { id: 2, owner: "" },
-    { id: 3, owner: "" },
-    { id: 4, owner: "" },
-    { id: 5, owner: "" },
-    { id: 6, owner: "" },
-    { id: 7, owner: "" },
-    { id: 8, owner: "" },
-    { id: 9, owner: "" },
-    { id: 10, owner: "" },
-    { id: 11, owner: "" },
-    { id: 12, owner: "" },
-    { id: 13, owner: "" },
-    { id: 14, owner: "" },
-    { id: 15, owner: "" },
+    { id: 0, owner: "", imgSrc: signs[0] },
+    { id: 1, owner: "", imgSrc: signs[0] },
+    { id: 2, owner: "", imgSrc: signs[0] },
+    { id: 3, owner: "", imgSrc: signs[0] },
+    { id: 4, owner: "", imgSrc: signs[0] },
+    { id: 5, owner: "", imgSrc: signs[0] },
+    { id: 6, owner: "", imgSrc: signs[0] },
+    { id: 7, owner: "", imgSrc: signs[0] },
+    { id: 8, owner: "", imgSrc: signs[0] },
+    { id: 9, owner: "", imgSrc: signs[0] },
+    { id: 10, owner: "", imgSrc: signs[0] },
+    { id: 11, owner: "", imgSrc: signs[0] },
+    { id: 12, owner: "", imgSrc: signs[0] },
+    { id: 13, owner: "", imgSrc: signs[0] },
+    { id: 14, owner: "", imgSrc: signs[0] },
+    { id: 15, owner: "", imgSrc: signs[0] },
   ]);
   const userX = () => {
     setUserSign("X");
@@ -49,11 +51,11 @@ export default function XO() {
         new Array(16).fill(null).map((square, idx) =>
           (idx + 1) % 4 !== 0 ? (
             <div style={{ display: "inline", margin: "2px" }}>
-              {<Square imgSrc={S} idx={squares[idx].id} />}
+              {<Square imgSrc={squares[idx].imgSrc} idx={squares[idx].id} signs={signs} userSign={userSign} setSquares={setSquares} />}
             </div>
           ) : (
             <div style={{ display: "inline", margin: "2px" }}>
-              {<Square imgSrc={S} idx={squares[idx].id} />}
+              {<Square imgSrc={squares[idx].imgSrc} idx={squares[idx].id} signs={signs} userSign={userSign} setSquares={setSquares} />}
               <br></br>
             </div>
           )
