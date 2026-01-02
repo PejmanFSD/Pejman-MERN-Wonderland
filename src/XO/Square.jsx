1bd5c2ee8e5b7142623f6c1baa39b50ccde70ff4
Pejmanfsd <pejmanfsd@gmail.com>
2026-01-02 00:22:53 -0500

    Creating the board of the game.
    A board with 16 empty squares is created.

---
+++ src/XO/Square.jsx
@@ -0,0 +1,5 @@
+export default function Square({imgSrc}) {
+    return (
+        <img src={imgSrc} width="50px" alt="Square" style={{display: "inline"}} />
+    )
+}
--- src/XO/XO.jsx
+++ src/XO/XO.jsx
@@ -1,5 +1,23 @@
+import Square from "./Square";
+import S from "./Images/S.jpg";
+import X from "./Images/X.jpg";
+import O from "./Images/O.jpg";
+
 export default function XO() {
-    return (
-        <div>X-O</div>
-    )
+  return (
+    <div>
+      {new Array(16).fill(null).map((square, idx) =>
+        (idx + 1) % 4 !== 0 ? (
+          <div style={{ display: "inline", margin: "2px" }}>
+            {<Square imgSrc={S} />}
+          </div>
+        ) : (
+          <div style={{ display: "inline", margin: "2px" }}>
+            {<Square imgSrc={S} />}
+            <br></br>
+          </div>
+        )
+      )}
+    </div>
+  );
 }
