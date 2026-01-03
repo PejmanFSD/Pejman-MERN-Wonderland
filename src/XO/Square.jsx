770016d220429aa450b1c1c3c256b889f5ab3b84
Pejmanfsd <pejmanfsd@gmail.com>
2026-01-02 20:32:18 -0500

    Implementing the "Easy" mode and "Normal" mode + allowing the user to choose only one square.
    The user can choose to play in either "Easy" mode or "Normal" mode. Also when the user clicks on one of the squares in their turn, they can't click on another square.

--- src/XO/Square.jsx
+++ src/XO/Square.jsx
@@ -1,13 +1,27 @@
-export default function Square({ imgSrc, idx, signs, userSign, setSquares }) {
+export default function Square({
+  imgSrc,
+  idx,
+  signs,
+  userSign,
+  setSquares,
+  isUserTurn,
+  setIsUserTurn,
+}) {
   const handleClickSquare = () => {
     if (userSign === "X") {
       setSquares((currSquares) =>
-        currSquares.map((s) => (s.id === idx ? { ...s, imgSrc: signs[1] } : s))
+        currSquares.map((s) =>
+          s.id === idx ? { ...s, imgSrc: signs[1], owner: "User" } : s
+        )
       );
+      setIsUserTurn(false);
     } else if (userSign === "O") {
       setSquares((currSquares) =>
-        currSquares.map((s) => (s.id === idx ? { ...s, imgSrc: signs[2] } : s))
+        currSquares.map((s) =>
+          s.id === idx ? { ...s, imgSrc: signs[2], owner: "User" } : s
+        )
       );
+      setIsUserTurn(false);
     }
   };
   return (
@@ -15,7 +29,7 @@ export default function Square({ imgSrc, idx, signs, userSign, setSquares }) {
       src={imgSrc}
       width="50px"
       alt="Square"
-      style={{ display: "inline" }}
+      style={{ display: "inline", pointerEvents: !isUserTurn && "none" }}
       onClick={handleClickSquare}
     />
   );
--- src/XO/XO.jsx
+++ src/XO/XO.jsx
@@ -7,8 +7,9 @@ const signs = [S, X, O];
 
 export default function XO() {
   const [isGameStarted, setIsGameStarted] = useState(false);
+  const [easyMode, setEasyMode] = useState(false);
+  const [normalMode, setNormalMode] = useState(false);
   const [userSign, setUserSign] = useState("");
-//   const [imgSrc, setImgSrc] = useState(signs[0]);
   const [squares, setSquares] = useState([
     { id: 0, owner: "", imgSrc: signs[0] },
     { id: 1, owner: "", imgSrc: signs[0] },
@@ -26,18 +27,45 @@ export default function XO() {
     { id: 13, owner: "", imgSrc: signs[0] },
     { id: 14, owner: "", imgSrc: signs[0] },
     { id: 15, owner: "", imgSrc: signs[0] },
+    { id: 16, owner: "", imgSrc: signs[0] },
+    { id: 17, owner: "", imgSrc: signs[0] },
+    { id: 18, owner: "", imgSrc: signs[0] },
+    { id: 19, owner: "", imgSrc: signs[0] },
+    { id: 20, owner: "", imgSrc: signs[0] },
+    { id: 21, owner: "", imgSrc: signs[0] },
+    { id: 22, owner: "", imgSrc: signs[0] },
+    { id: 23, owner: "", imgSrc: signs[0] },
+    { id: 24, owner: "", imgSrc: signs[0] },
   ]);
+  const [isUserTurn, setIsUserTurn] = useState(false);
   const userX = () => {
     setUserSign("X");
-    setIsGameStarted(true);
   };
   const userO = () => {
     setUserSign("O");
+  };
+  const handleEasyMode = () => {
+    setEasyMode(true);
+    setNormalMode(false);
+    setIsUserTurn(true);
+  };
+  const handleNormalMode = () => {
+    setNormalMode(true);
+    setEasyMode(false);
+    setIsUserTurn(false);
+  };
+  const handleStart = () => {
     setIsGameStarted(true);
   };
   return (
     <div>
-      {!isGameStarted && (
+      {!easyMode && !normalMode && (
+        <div>
+          <button onClick={handleEasyMode}>Easy</button>
+          <button onClick={handleNormalMode}>Normal</button>
+        </div>
+      )}
+      {userSign === "" && (easyMode || normalMode) && (
         <div>
           Choose One:
           <div>
@@ -46,16 +74,40 @@ export default function XO() {
           </div>
         </div>
       )}
+      {userSign !== "" && !isGameStarted && (
+        <button onClick={handleStart}>Start the Game</button>
+      )}
       <div style={{ color: "gray" }}>User's Sign: {userSign}</div>
+      <div style={{ color: "gray" }}>isUserTurn: {isUserTurn ? "T" : "F"}</div>
       {isGameStarted &&
-        new Array(16).fill(null).map((square, idx) =>
-          (idx + 1) % 4 !== 0 ? (
+        new Array(25).fill(null).map((square, idx) =>
+          (idx + 1) % 5 !== 0 ? (
             <div style={{ display: "inline", margin: "2px" }}>
-              {<Square imgSrc={squares[idx].imgSrc} idx={squares[idx].id} signs={signs} userSign={userSign} setSquares={setSquares} />}
+              {
+                <Square
+                  imgSrc={squares[idx].imgSrc}
+                  idx={squares[idx].id}
+                  signs={signs}
+                  userSign={userSign}
+                  setSquares={setSquares}
+                  isUserTurn={isUserTurn}
+                  setIsUserTurn={setIsUserTurn}
+                />
+              }
             </div>
           ) : (
             <div style={{ display: "inline", margin: "2px" }}>
-              {<Square imgSrc={squares[idx].imgSrc} idx={squares[idx].id} signs={signs} userSign={userSign} setSquares={setSquares} />}
+              {
+                <Square
+                  imgSrc={squares[idx].imgSrc}
+                  idx={squares[idx].id}
+                  signs={signs}
+                  userSign={userSign}
+                  setSquares={setSquares}
+                  isUserTurn={isUserTurn}
+                  setIsUserTurn={setIsUserTurn}
+                />
+              }
               <br></br>
             </div>
           )
