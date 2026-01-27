7a369afc152076dbbb86c79c634add004b05b08e
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-01-27 16:26:24 -0500

    Choosing one square by the user.
    The user can choose one of the squares.

--- client/src/KukuKube/KukuKube.jsx
+++ client/src/KukuKube/KukuKube.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import Square from "./Square";
 import ModeExplaination from "../ModeExplaination";
-import {getRandNum} from "../utils";
+import { getRandNum } from "../utils";
 
 export default function KukuKube() {
   const [easyMode, setEasyMode] = useState(false);
   const [normalMode, setNormalMode] = useState(false);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [isColorChosen, setIsColorChosen] = useState(false);
-  const [color, setColor] = useState({red: null, green: null, blue: null, opacity: null});
+  const [color, setColor] = useState({ red: null, green: null, blue: null });
   const [squareNum, setSquareNum] = useState(0);
   const [uniqueSquare, setUniqueSquare] = useState(0);
+  const [step, setStep] = useState(0);
+  const [userChoice, setUserChoice] = useState(null);
 
   const runEasyMode = () => {
     setEasyMode(true);
@@ -25,91 +27,90 @@ export default function KukuKube() {
   const handleStart = () => {
     setIsGameStarted(true);
     setUniqueSquare(getRandNum(squareNum));
+    setStep(1);
   };
   const handleColor = (e) => {
-    if (e.target.value === 'Red') {
-        setColor({red: 255, green: 0, blue: 0, opacity: 1});
-    } else if (e.target.value === 'Green') {
-        setColor({red: 0, green: 170, blue: 0, opacity: 1});
-    } else if (e.target.value === 'Blue') {
-        setColor({red: 0, green: 0, blue: 255, opacity: 1});
+    if (e.target.value === "Red") {
+      setColor({ red: 170, green: 0, blue: 0 });
+    } else if (e.target.value === "Green") {
+      setColor({ red: 0, green: 170, blue: 0 });
+    } else if (e.target.value === "Blue") {
+      setColor({ red: 0, green: 0, blue: 170 });
     }
     setIsColorChosen(true);
-  }
+  };
   return (
-  <div>
-    <h2>Kuku Kube</h2>
-    <h4>In each step, find the square with the unique color</h4>
-    {!isGameStarted && !easyMode && !normalMode &&
     <div>
-        <button onClick={runEasyMode}>Easy Mode</button>
-        <button onClick={runNormalMode}>Normal Mode</button>
-    </div>
-    }
-    {easyMode && !normalMode
+      <h2>Kuku Kube</h2>
+      <h4>In each step, find the square with the unique color</h4>
+      {!isGameStarted && !easyMode && !normalMode && (
+        <div>
+          <button onClick={runEasyMode}>Easy Mode</button>
+          <button onClick={runNormalMode}>Normal Mode</button>
+        </div>
+      )}
+      {easyMode && !normalMode ? (
         //   && !isTogglingReset &&
         //   !isTogglingHomePage &&
         //   !isTogglingLevel
-          ? (
-            <ModeExplaination message="Easy Mode: You won't get any stars if you win." />
-          ) : (
-            !easyMode && normalMode
-            // && !isTogglingReset &&
-            // !isTogglingHomePage &&
-            // !isTogglingLevel
-            && (
-              <ModeExplaination message="Normal Mode: You will get one star if you win." />
-            )
-          )}
-    {!isGameStarted && (easyMode || normalMode) &&
+        <ModeExplaination message="Easy Mode: You won't get any stars if you win." />
+      ) : (
+        !easyMode &&
+        normalMode && (
+          // && !isTogglingReset &&
+          // !isTogglingHomePage &&
+          // !isTogglingLevel
+          <ModeExplaination message="Normal Mode: You will get one star if you win." />
+        )
+      )}
+      {!isGameStarted && (easyMode || normalMode) && (
         <div>
-          <label htmlFor='color'></label>
-          <select
-            onChange={handleColor}
-            name='color'
-            id='color'
-          >
+          <label htmlFor="color"></label>
+          <select onChange={handleColor} name="color" id="color">
             <option value={color} disabled selected>
               Select a Color
             </option>
-            {['Red', 'Green', 'Blue'].map((c) => (
+            {["Red", "Green", "Blue"].map((c) => (
               <option>{c}</option>
             ))}
           </select>
         </div>
-    }
-    {!isGameStarted && (easyMode || normalMode) && isColorChosen &&
+      )}
+      {!isGameStarted && (easyMode || normalMode) && isColorChosen && (
         <button onClick={handleStart}>Start the Game</button>
-    }
-    <div>{uniqueSquare}</div>
-    {isGameStarted &&
-    new Array(squareNum).fill(null).map((el, idx) =>
-        (idx+1) % (squareNum ** 0.5) !== 0 ?
-        (
+      )}
+      <div style={{ color: "gray" }}>The chosen square: {uniqueSquare}</div>
+      <div style={{ color: "gray" }}>User's choice: {userChoice}</div>
+      {step > 0 && <div>Step {step}</div>}
+      {isGameStarted &&
+        new Array(squareNum).fill(null).map((el, idx) =>
+          (idx + 1) % squareNum ** 0.5 !== 0 ? (
             <div style={{ display: "inline" }}>
-                <Square
+              <Square
                 easyMode={easyMode}
                 red={color.red}
                 green={color.green}
                 blue={color.blue}
-                opacity={idx + 1 === uniqueSquare ? 0.5 : 1}
-                text={idx + 1} />
+                opacity={idx + 1 === uniqueSquare ? step * 0.085 : 1}
+                text={idx + 1}
+                setUserChoice={setUserChoice}
+              />
             </div>
-        ) : (
+          ) : (
             <div style={{ display: "inline" }}>
-                <Square
+              <Square
                 easyMode={easyMode}
                 red={color.red}
                 green={color.green}
                 blue={color.blue}
-                opacity={idx + 1 === uniqueSquare ? 0.5 : 1}
-                text={idx + 1} />
-                <br></br>
+                opacity={idx + 1 === uniqueSquare ? step * 0.085 : 1}
+                text={idx + 1}
+                setUserChoice={setUserChoice}
+              />
+              <br></br>
             </div>
-            
-        )
-    )
-    }
-  </div>
-  )
+          ),
+        )}
+    </div>
+  );
 }
--- client/src/KukuKube/Square.jsx
+++ client/src/KukuKube/Square.jsx
@@ -1,18 +1,30 @@
-export default function Square({easyMode, red, green, blue, opacity, text}) {
-    return (
-        <button
-        style={{
-            // display: "inline",
-            border: "1px solid black",
-            margin: easyMode ? "6px" : "3px",
-            width: easyMode ? "80px" : "40px",
-            height: easyMode ? "80px" : "40px",
-            position: "relative",
-            top: "20px",
-            backgroundColor: `rgba(${red}, ${green}, ${blue}, ${opacity})`
-            }}
-        >
-            {text}
-        </button>
-    )
+export default function Square({
+  easyMode,
+  red,
+  green,
+  blue,
+  opacity,
+  setUserChoice,
+  text,
+}) {
+  const handleClick = () => {
+    setUserChoice(text);
+  };
+  return (
+    <button
+      style={{
+        // display: "inline",
+        border: "1px solid black",
+        margin: easyMode ? "6px" : "3px",
+        width: easyMode ? "80px" : "40px",
+        height: easyMode ? "80px" : "40px",
+        position: "relative",
+        top: "20px",
+        backgroundColor: `rgba(${red}, ${green}, ${blue}, ${opacity})`,
+      }}
+      onClick={handleClick}
+    >
+      {/* {text} */}
+    </button>
+  );
 }
