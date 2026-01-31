53f92a4e0b1aa8ae1706c79375b161d5a42da39c
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-01-31 13:17:11 -0500

    Implementing the "Reset" process.
    The user can reset the game whenever they want.

--- client/src/KukuKube/KukuKube.jsx
+++ client/src/KukuKube/KukuKube.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Square from "./Square";
 import ModeExplaination from "../ModeExplaination";
+import ConfirmationBox from "../ConfirmationBox";
 import { getRandNum } from "../utils";
 import Blank from './images/Blank.jpg';
 import Cross from './images/Cross.jpg';
@@ -19,6 +20,7 @@ export default function KukuKube({updateTotalPoint}) {
   const [userChoice, setUserChoice] = useState(null);
   const [isStepPassed, setIsStepPassed] = useState(null);
   const [isUniqueSquareRevealed, setIsUniqueSquareRevealed] = useState(false);
+  const [isTogglingReset, setIsTogglingReset] = useState(false);
 
   const runEasyMode = () => {
     setEasyMode(true);
@@ -35,6 +37,33 @@ export default function KukuKube({updateTotalPoint}) {
     setUniqueSquare(getRandNum(squareNum));
     setStep(1);
   };
+    const handleReset = () => {
+    setStep(1);
+    setUserChoice(null);
+    setUniqueSquare(getRandNum(squareNum));
+    setIsStepPassed(null);
+    setIsUniqueSquareRevealed(false);
+  }
+  const toggleReset = () => {
+    setIsTogglingReset(true);
+  };
+  const toggleResetYes = () => {
+    setIsColorChosen(false);
+    setColor({ red: null, green: null, blue: null });
+    setEasyMode(false);
+    setNormalMode(false);
+    setSquareNum(0);
+    setUniqueSquare(0);
+    setStep(0);
+    setUserChoice(null);
+    setIsStepPassed(null);
+    setIsUniqueSquareRevealed(false);
+    setIsGameStarted(false);
+    setIsTogglingReset(false);
+  };
+    const toggleResetCancel = () => {
+    setIsTogglingReset(false);
+  };
   const handleColor = (e) => {
     if (e.target.value === "Red") {
       setColor({ red: 170, green: 0, blue: 0 });
@@ -49,7 +78,7 @@ export default function KukuKube({updateTotalPoint}) {
     setIsUniqueSquareRevealed(true);
     if (userChoice === uniqueSquare) {
         setIsStepPassed(true);
-        if (step === 10 && normalMode) {
+        if (step === 12 && normalMode) {
             updateTotalPoint(1);
         }
     }
@@ -64,31 +93,24 @@ export default function KukuKube({updateTotalPoint}) {
     setIsStepPassed(null);
     setIsUniqueSquareRevealed(false);
   }
-  const handleTryAgain = () => {
-    setStep(1);
-    setUserChoice(null);
-    setUniqueSquare(getRandNum(squareNum));
-    setIsStepPassed(null);
-    setIsUniqueSquareRevealed(false);
-  }
   return (
     <div>
       <h2>Kuku Kube</h2>
-      <h4>In each step, find the square with the unique color</h4>
+      {!isTogglingReset && <h4>In each step, find the square with the unique color</h4>}
       {!isGameStarted && !easyMode && !normalMode && (
         <div>
           <button onClick={runEasyMode}>Easy Mode</button>
           <button onClick={runNormalMode}>Normal Mode</button>
         </div>
       )}
-      {easyMode && !normalMode ? (
+      {easyMode && !normalMode && !isTogglingReset ? (
         //   && !isTogglingReset &&
         //   !isTogglingHomePage &&
         //   !isTogglingLevel
         <ModeExplaination message="Easy Mode: You won't get any stars if you win." />
       ) : (
         !easyMode &&
-        normalMode && (
+        normalMode && !isTogglingReset && (
           // && !isTogglingReset &&
           // !isTogglingHomePage &&
           // !isTogglingLevel
@@ -113,8 +135,25 @@ export default function KukuKube({updateTotalPoint}) {
       )}
       <div style={{ color: "gray" }}>The chosen square: {uniqueSquare}</div>
       <div style={{ color: "gray" }}>User's choice: {userChoice}</div>
-      {step > 0 && <div>Step {step}</div>}
       {isGameStarted &&
+        !isTogglingReset &&
+        // !isTogglingHomePage &&
+        (easyMode || normalMode) && (
+          <div>
+            <button onClick={toggleReset}>Reset the Game</button>
+          </div>
+        )}
+      {isTogglingReset && (
+              <div>
+                <ConfirmationBox
+                  question="Are you sure you want to reset the game?"
+                  toggleYes={toggleResetYes}
+                  toggleCancel={toggleResetCancel}
+                />
+              </div>
+            )}
+      {step > 0 && !isTogglingReset && <div>Step {step}</div>}
+      {isGameStarted && !isTogglingReset &&
         new Array(squareNum).fill(null).map((el, idx) =>
           (idx + 1) % squareNum ** 0.5 !== 0 ? (
             <div style={{ display: "inline" }}>
@@ -123,7 +162,7 @@ export default function KukuKube({updateTotalPoint}) {
                 red={color.red}
                 green={color.green}
                 blue={color.blue}
-                opacity={idx + 1 === uniqueSquare ? step * 0.085 : 1}
+                opacity={idx + 1 === uniqueSquare ? step * 0.072 : 1}
                 text={idx + 1}
                 userChoice={userChoice}
                 setUserChoice={setUserChoice}
@@ -139,7 +178,7 @@ export default function KukuKube({updateTotalPoint}) {
                 red={color.red}
                 green={color.green}
                 blue={color.blue}
-                opacity={idx + 1 === uniqueSquare ? step * 0.085 : 1}
+                opacity={idx + 1 === uniqueSquare ? step * 0.072 : 1}
                 text={idx + 1}
                 userChoice={userChoice}
                 setUserChoice={setUserChoice}
@@ -151,10 +190,10 @@ export default function KukuKube({updateTotalPoint}) {
             </div>
           ),
         )}
-        {isGameStarted &&
-          new Array(10).fill(null).map((el, idx) =>
+        {isGameStarted && !isTogglingReset &&
+          new Array(12).fill(null).map((el, idx) =>
             <img
-            style={{height: "20px", position: "relative", top: "30px", margin: "2px"}}
+            style={{height: "19px", position: "relative", top: "30px", margin: "2px"}}
             src={
               idx === step - 1 && isUniqueSquareRevealed && userChoice === uniqueSquare ? Tick :
               idx === step - 1 && isUniqueSquareRevealed && userChoice !== uniqueSquare ? Cross :
@@ -166,34 +205,34 @@ export default function KukuKube({updateTotalPoint}) {
           )
         }
         <br></br>
-        {isGameStarted && <button
+        {isGameStarted && !isTogglingReset && <button
         onClick={submitUserChoice}
         style={{position: "relative", top: "50px"}}
         disabled={!userChoice || isStepPassed !== null}
         >
             Submit
         </button>}
-        {isGameStarted && isStepPassed === true && userChoice && step !== 10 &&
+        {isGameStarted && isStepPassed === true && userChoice && step !== 12 && !isTogglingReset &&
             <div style={{position: "relative", top: "60px"}}>
                 Well Done! You guessed correctly!
                 <div><button onClick={handleNextStep}>{`Go to Step ${step + 1}`}</button></div>
             </div>
         }
-        {isGameStarted && isStepPassed === true && userChoice && step === 10 &&
+        {isGameStarted && isStepPassed === true && userChoice && step === 12 && !isTogglingReset &&
             <div style={{position: "relative", top: "60px"}}>
               {`You Win! ${normalMode ? "You get 1 star :)" : "But you won't get any stars :("}`}
                 <div>
                     <div>Play Again?</div>
-                    <button onClick={handleTryAgain}>Ok</button>
+                    <button onClick={handleReset}>Ok</button>
                 </div>
             </div>
         }
-        {isGameStarted && isStepPassed === false && userChoice &&
+        {isGameStarted && isStepPassed === false && userChoice && !isTogglingReset &&
             <div style={{position: "relative", top: "60px"}}>
                 Sorry! You didn't guess correctly!
                 <div>
                     <div>Try Again?</div>
-                    <button onClick={handleTryAgain}>Ok</button>
+                    <button onClick={handleReset}>Ok</button>
                 </div>
             </div>
         }
