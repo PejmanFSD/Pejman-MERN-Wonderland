5fa85278f1048a9d750f8710a03c89363baf37d0
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-02-05 12:21:19 -0500

    Back-End: Continuing the "routes" process.
    Creating the routes of "ads".

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const User = require('./models/user');
-const Ad = require('./models/ad');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const flash = require('connect-flash');
+// const flash = require('connect-flash');
 // const bcrypt = require('bcrypt');
 const session = require('express-session');
 const sessionOptions = {secret: 'Pejman-MERN-Wonderland', resave: false, saveUninitialized: false};
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
-const {adSchema} = require('./schemas.js');
+const ads = require('./routes/ads.js');
 
 mongoose.connect('mongodb://127.0.0.1:27017/pejman-mern-wonderland')
     .then(() => {
@@ -28,27 +27,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })); // For parcing the request
 app.use(methodOverride('_method'));
 app.use(session(sessionOptions));
-app.use(flash());
-app.use((req, res, next) => {
-    res.locals.messages = req.flash("success");
-    next();
-})
+app.use('/ads', ads);
+// app.use(flash());
+// app.use((req, res, next) => {
+//     res.locals.messages = req.flash("success");
+//     next();
+// })
 
 const requireLogin = (req, res, next) => {
     if (!req.session.user_id) {
         return res.redirect('/login');
     }
     next();
-}
-
-const validateAd = (req, res, next) => {
-    const {error} = adSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
 }
 
 app.get('/', (req, res) => {
@@ -64,7 +54,7 @@ app.post('/register', catchAsync(async (req, res) => {
     const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
-    req.flash('success', 'Successfully registered!');
+    // req.flash('success', 'Successfully registered!');
     res.redirect('/');
 }))
 
@@ -77,7 +67,7 @@ app.post('/login', catchAsync(async (req, res) => {
     const foundUser = await User.findAndValidate(username, password);
     if (foundUser) {
         req.session.user_id = foundUser._id;
-        req.flash('success', 'Successfully logged-in!');
+        // req.flash('success', 'Successfully logged-in!');
         res.redirect('./secret');
     } else {
         res.redirect('/login');
@@ -86,58 +76,9 @@ app.post('/login', catchAsync(async (req, res) => {
 
 app.post('/logout', (req, res) => {
     req.session.user_id = null;
-    req.flash('success', 'Successfully logged-out!');
+    // req.flash('success', 'Successfully logged-out!');
     res.redirect('/login');
 })
-// Ad route-handlers:
-app.get('/ads', catchAsync(async (req, res) => {
-    const ads = await Ad.find({});
-    res.render('ads/index', {ads});
-}))
-
-app.get('/ads/new', (req, res) => {
-    res.render('ads/new');
-})
-
-app.post('/ads', validateAd, catchAsync(async(req, res) => {
-    // if (!req.body.ad) throw new ExpressError('Invalid Ad Data', 400);
-    const ad = new Ad(req.body.ad);
-    await ad.save();
-    req.flash('success', 'Successfully made new Ad!');
-    res.redirect(`/ads/${ad._id}`);
-}))
-
-app.get('/ads/:id', catchAsync(async (req, res) => {
-    const {id} = req.params;
-    const ad = await Ad.findById(id);
-    if (!ad) {
-        throw new ExpressError('Ad not found!', 404);
-    }
-    res.render('ads/show', {ad});
-}))
-
-app.get('/ads/:id/edit', catchAsync(async(req, res) => {
-    const id = req.params.id;
-    const ad = await Ad.findById(id);
-    if (!ad) {
-        throw new ExpressError('Ad not found!', 404);
-    }
-    res.render('ads/edit', {ad});
-}))
-
-app.put('/ads/:id', validateAd, catchAsync(async(req, res) => {
-    const {id} = req.params;
-    const ad = await Ad.findByIdAndUpdate(id, {...req.body.ad}, {runValidators: true, new: true});
-    req.flash('success', 'Ad successfully edited!');
-    res.redirect(`/ads/${ad._id}`);
-}))
-
-app.delete('/ads/:id', catchAsync(async (req, res) => {
-    const {id} = req.params;
-    await Ad.findByIdAndDelete(id);
-    req.flash('success', 'Ad successfully deleted!');
-    res.redirect('/ads');
-}))
 
 app.get('/users', catchAsync(async (req, res) => {
     const users = await User.find({});
--- server/routes/ads.js
+++ server/routes/ads.js
@@ -1,3 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const Ad = require('../models/ad');
+const {adSchema} = require('../schemas.js');
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
 
+const validateAd = (req, res, next) => {
+    const {error} = adSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+router.get('/', catchAsync(async (req, res) => {
+    const ads = await Ad.find({});
+    res.render('ads/index', {ads});
+}))
+
+router.get('/new', (req, res) => {
+    res.render('ads/new');
+})
+
+router.post('/', validateAd, catchAsync(async(req, res) => {
+    // if (!req.body.ad) throw new ExpressError('Invalid Ad Data', 400);
+    const ad = new Ad(req.body.ad);
+    await ad.save();
+    // req.flash('success', 'Successfully made new Ad!');
+    res.redirect(`/ads/${ad._id}`);
+}))
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const {id} = req.params;
+    const ad = await Ad.findById(id);
+    if (!ad) {
+        throw new ExpressError('Ad not found!', 404);
+    }
+    res.render('ads/show', {ad});
+}))
+
+router.get('/:id/edit', catchAsync(async(req, res) => {
+    const id = req.params.id;
+    const ad = await Ad.findById(id);
+    if (!ad) {
+        throw new ExpressError('Ad not found!', 404);
+    }
+    res.render('ads/edit', {ad});
+}))
+
+router.put('/:id', validateAd, catchAsync(async(req, res) => {
+    const {id} = req.params;
+    const ad = await Ad.findByIdAndUpdate(id, {...req.body.ad}, {runValidators: true, new: true});
+    // req.flash('success', 'Ad successfully edited!');
+    res.redirect(`/ads/${ad._id}`);
+}))
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const {id} = req.params;
+    await Ad.findByIdAndDelete(id);
+    // req.flash('success', 'Ad successfully deleted!');
+    res.redirect('/ads');
+}))
+
+module.exports = router;
