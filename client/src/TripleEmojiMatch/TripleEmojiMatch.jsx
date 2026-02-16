4722177ff146b3603457cf8f4fa93be282f0d2ac
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-02-16 14:02:46 -0500

    Assigning the game modes and the start button.
    The user can choose either "Easy" mode or "Normal" mode and then start the game.

--- client/src/TripleEmojiMatch/TripleEmojiMatch.jsx
+++ client/src/TripleEmojiMatch/TripleEmojiMatch.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import ModeExplaination from "../ModeExplaination";
+
 export default function TripleEmojiMatch() {
-    return (
-        <div>Triple Emoji Match</div>
-    )
+  const [easyMode, setEasyMode] = useState(false);
+  const [normalMode, setNormalMode] = useState(false);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const runEasyMode = () => {
+    setEasyMode(true);
+    setNormalMode(false);
+    setIsGameStarted(true);
+  };
+  const runNormalMode = () => {
+    setNormalMode(true);
+    setEasyMode(false);
+    setIsGameStarted(true);
+  };
+
+  return (
+    <div>
+      <h2>Triple Emoji Match</h2>
+      {!isGameStarted && !easyMode && !normalMode && (
+        <div>
+          <button onClick={runEasyMode}>Easy Mode</button>
+          <button onClick={runNormalMode}>Normal Mode</button>
+        </div>
+      )}
+      {easyMode && !normalMode ? (
+        <ModeExplaination message="Easy Mode: There's no timer. You won't get any stars if you win." />
+      ) : (
+        !easyMode &&
+        normalMode && (
+          <ModeExplaination message="Normal Mode: Find all the matches in *** seconds. You will get one star if you win." />
+        )
+      )}
+      {isGameStarted &&
+        <div>
+            Triple Emoji Match
+        </div>
+        }
+    </div>
+  );
 }
