41a0ea8f4ebf10d56bc401b528e86f54c14145d6
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-02-17 14:50:01 -0500

    Front-End: "Triple Emoji Match" game.
    Creating the emojis state variable.

--- server/index.js
+++ server/index.js
@@ -92,6 +92,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("SERVING YOUR APP! - 3000");
 })
