6b5cba9f17a0ace3675378abddb45b603dc54834
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-02-18 15:44:23 -0500

    Catching the triple matches.
    If the user chooses 3 identical emojis, they disappear both from the board and the selected emojis area.

--- client/src/TripleEmojiMatch/Emoji.jsx
+++ client/src/TripleEmojiMatch/Emoji.jsx
@@ -1,32 +1,49 @@
 import E00 from "./images/000.jpg";
 
-export default function Emoji({imgId, imgSrc, emojis, setEmojis, selectedEmojis, setSelectedEmojis}) {
-    const handleClickEmoji = () => {
-        setEmojis((currEmojis) =>
-            currEmojis.map((emoji) =>
-                emoji.id === imgId ? {...emoji, image: E00, isSelected: true} : emoji
-            )
+export default function Emoji({
+  imgId,
+  imgSrc,
+  emojis,
+  setEmojis,
+  selectedEmojis,
+  setSelectedEmojis,
+}) {
+  const handleClickEmoji = () => {
+    setEmojis((currEmojis) =>
+      currEmojis.map((emoji) =>
+        emoji.id === imgId ? { ...emoji, image: E00, isSelected: true } : emoji,
+      ),
+    );
+    for (const selectedEmoji of selectedEmojis) {
+      if (selectedEmoji.isFilled === false) {
+        setSelectedEmojis((currSelectedEmojis) =>
+          currSelectedEmojis.map((emoji) =>
+            emoji.id === selectedEmoji.id
+              ? {
+                  ...emoji,
+                  image: emojis.find((currEmoji) => currEmoji.id === imgId)
+                    .image,
+                  isFilled: true,
+                }
+              : emoji,
+          ),
         );
-        for (const selectedEmoji of selectedEmojis) {
-            if (selectedEmoji.isFilled === false) {
-                setSelectedEmojis((currSelectedEmojis) =>
-                    currSelectedEmojis.map((emoji) =>
-                        emoji.id === selectedEmoji.id ?
-                        {...emoji, image: emojis.find((currEmoji) => currEmoji.id === imgId).image,  isFilled: true} :
-                        emoji
-                    )
-                )
-                break;
-            }
-        }
+        break;
+      }
     }
-    return (
-        <div>
-            <img
-                src={imgSrc}
-                style={{width: "30px", border: "1px solid black", marginLeft: "4px"}}
-                onClick={handleClickEmoji}
-            />
-        </div>
-    )
+  };
+  return (
+    <div>
+      <img
+        src={imgSrc}
+        style={{
+          width: "30px",
+          border: "1px solid black",
+          marginLeft: "4px",
+          pointerEvents: imgSrc === E00 ? "none" : "",
+        }}
+        onClick={handleClickEmoji}
+      />
+    </div>
+  );
 }
