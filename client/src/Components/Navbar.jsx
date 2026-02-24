6a08ceea967283c28ab580ed0d1692224190cd22
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-02-24 13:01:41 -0500

    Hiding the <button /> of the current page.
    The <button /> of the page that we're visiting should be hidden.

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -4,29 +4,36 @@ import {
   Route,
   Link,
   useNavigate,
+  useLocation, // For hiding the button of the current page
 } from "react-router-dom";
 import AdForm from "./ads/AdForm";
 
 export default function Navbar({ ads, setAds }) {
   const navigate = useNavigate();
+  const location = useLocation();
+  //   "location.pathname" is the path of the current page
   return (
     <header>
-        <h1>Pejman MERN Wonderland</h1>
+      <h1>Pejman MERN Wonderland</h1>
+      {location.pathname !== "/" && (
         <button onClick={() => navigate("/")}>Home Page</button>
+      )}
+      {location.pathname !== "/newAd" && (
         <button onClick={() => navigate("/newAd")}>Create new Ad</button>
-        <Routes>
-          <Route
-            path="/newAd"
-            element={
-              <AdForm
-                onAdCreated={(newAd) => {
-                  setAds((currAds) => [newAd, ...currAds]);
-                }}
-              />
-            }
-          />
-        </Routes>
-        <hr />
+      )}
+      <Routes>
+        <Route
+          path="/newAd"
+          element={
+            <AdForm
+              onAdCreated={(newAd) => {
+                setAds((currAds) => [newAd, ...currAds]);
+              }}
+            />
+          }
+        />
+      </Routes>
+      <hr />
     </header>
   );
 }
