6c96d35ee5bbb3b7930daeaa1a8cd8f2f32a1129
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-03-06 11:45:13 -0500

    Validating the "currentPassword" in update profile.
    In the update profile page, in order to update password, the user first should enter their current password correctly.

--- client/src/Components/users/EditProfile.jsx
+++ client/src/Components/users/EditProfile.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-export default function EditProfile({setCurrentUser}) {
+export default function EditProfile({ setCurrentUser }) {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -30,18 +31,21 @@ export default function EditProfile({setCurrentUser}) {
       credentials: "include",
       body: JSON.stringify({ username, message }),
     });
-    if (currentPassword && newPassword) {
-      await fetch("/users/change-password", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          currentPassword,
-          newPassword,
-        }),
-      });
+    const passwordResponse = await fetch("/users/change-password", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        currentPassword,
+        newPassword,
+      }),
+    });
+    const passwordData = await passwordResponse.json();
+    if (!passwordResponse.ok) {
+      setPasswordError(passwordData.message);
+      return;
     }
     if (response.ok) {
       const updatedUser = await response.json();
@@ -70,6 +74,7 @@ export default function EditProfile({setCurrentUser}) {
           required
         />
       </div>
+      <label>Password:</label>
       <input
         type="password"
         placeholder="Current Password"
@@ -83,6 +88,7 @@ export default function EditProfile({setCurrentUser}) {
         onChange={(e) => setNewPassword(e.target.value)}
       />
       <button type="submit">Save Changes</button>
+      {passwordError && <p style={{ color: "red" }}>{passwordError}</p>}
     </form>
   );
 }
