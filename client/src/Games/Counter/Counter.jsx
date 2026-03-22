8520185b69779956621fed84cef6526e6c85a6da
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-03-22 17:51:32 -0400

    Displaying the images of the game.
    Each image of the game may repeat twice or three times in a new generated array and displayed in UI.

--- client/src/Games/Counter/Counter.jsx
+++ client/src/Games/Counter/Counter.jsx
@@ -1,12 +1,48 @@
+import {useState, useEffect} from "react";
 import { imagesArray } from "./imagesArray";
+import {getRandNumInRange} from "../utils";
 
 export default function Counter() {
+    const [gameArray, setGameArray] = useState(imagesArray);
+    const [finalGameArray, setFinalGameArray] = useState([]);
+    const [isGameStarted, setIsGameStarted] = useState(false);
+    const handleStart = () => {
+        setIsGameStarted(true);
+        // expanding the gameArray based on the repetition value
+        // and store it into a temporary variable (expandedArray):
+        const expandedArray = gameArray.flatMap(item =>
+            Array.from({ length: item.repetition }, (_, i) => ({
+            ...item,
+            instanceId: `${item.id}-${i}`
+            }))
+        );
+        // Shuffling the new temporary variable (expandedArray):
+        const shuffled = expandedArray.sort(() => Math.random() - 0.5);
+        // Assigning the new temporary variable (expandedArray) to a
+        // permanent variable(finalGameArray):
+        setFinalGameArray(shuffled);
+    }
+    useEffect(() => {
+        setGameArray((currGameArray) => currGameArray.map(arr => (
+            {...arr, repetition: getRandNumInRange(1,3)}
+        )))
+    }, []);
     return (
         <div>
-            {imagesArray.map((el, i) =>
+            <button onClick={handleStart}>Start the Game</button>
+            <br />
+            {gameArray.map((el) =>
+                <div style={{display: "inline"}}>{el.repetition} - </div>
+            )}
+            <br />
+            {finalGameArray.map((el, i) =>
                 <img
-                    src={imagesArray[i].image}
-                    style={{width: "100px", border: "1px solid black"}}
+                    src={finalGameArray[i].image}
+                    style={{
+                        width: "80px",
+                        border: "1px solid black",
+                        margin: "5px"
+                    }}
                 />
             )}
         </div>
--- client/src/Games/Counter/imagesArray.js
+++ client/src/Games/Counter/imagesArray.js
@@ -1,22 +1,22 @@
 import { pics } from "./imagesGroup";
 
 const imagesArray = [
-    {id: 0, image: pics[0]},
-    {id: 1, image: pics[1]},
-    {id: 2, image: pics[2]},
-    {id: 3, image: pics[3]},
-    {id: 4, image: pics[4]},
-    {id: 5, image: pics[5]},
-    {id: 6, image: pics[6]},
-    {id: 7, image: pics[7]},
-    {id: 8, image: pics[8]},
-    {id: 9, image: pics[9]},
-    {id: 10, image: pics[10]},
-    {id: 11, image: pics[11]},
-    {id: 12, image: pics[12]},
-    {id: 13, image: pics[13]},
-    {id: 14, image: pics[14]},
-    {id: 15, image: pics[15]}
+    {id: 0, image: pics[0], repetition: 0},
+    {id: 1, image: pics[1], repetition: 0},
+    {id: 2, image: pics[2], repetition: 0},
+    {id: 3, image: pics[3], repetition: 0},
+    {id: 4, image: pics[4], repetition: 0},
+    {id: 5, image: pics[5], repetition: 0},
+    {id: 6, image: pics[6], repetition: 0},
+    {id: 7, image: pics[7], repetition: 0},
+    {id: 8, image: pics[8], repetition: 0},
+    {id: 9, image: pics[9], repetition: 0},
+    {id: 10, image: pics[10], repetition: 0},
+    {id: 11, image: pics[11], repetition: 0},
+    {id: 12, image: pics[12], repetition: 0},
+    {id: 13, image: pics[13], repetition: 0},
+    {id: 14, image: pics[14], repetition: 0},
+    {id: 15, image: pics[15], repetition: 0}
 ];
 
 export { imagesArray };
