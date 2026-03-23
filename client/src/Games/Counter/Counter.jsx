96b372737d37730a395fd58c7903abccf375ee53
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-03-23 10:54:11 -0400

    Creating the slideshow.
    The images are presented in a slide show format.

--- client/src/Games/Counter/Counter.jsx
+++ client/src/Games/Counter/Counter.jsx
@@ -6,6 +6,10 @@ export default function Counter() {
     const [gameArray, setGameArray] = useState(imagesArray);
     const [finalGameArray, setFinalGameArray] = useState([]);
     const [isGameStarted, setIsGameStarted] = useState(false);
+    // Variables for the timer:
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [showImage, setShowImage] = useState(true); // A boolean to show/hide images
+
     const handleStart = () => {
         setIsGameStarted(true);
         // expanding the gameArray based on the repetition value
@@ -27,6 +31,22 @@ export default function Counter() {
             {...arr, repetition: getRandNumInRange(1,3)}
         )))
     }, []);
+    useEffect(() => {
+        let step = 0;
+
+        const interval = setInterval(() => {
+            if (step % 2 === 0) {
+            setShowImage(true);
+            } else {
+            setShowImage(false);
+            setCurrentIndex(prev => prev + 1);
+            }
+
+            step++;
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
     return (
         <div>
             <button onClick={handleStart}>Start the Game</button>
@@ -44,6 +64,18 @@ export default function Counter() {
                         margin: "5px"
                     }}
                 />
+            )}
+            <br />
+            {showImage && finalGameArray[currentIndex] && (
+            <img
+                src={finalGameArray[currentIndex].image}
+                alt=""
+                style={{
+                    width: "80px",
+                    border: "1px solid black",
+                    margin: "5px"
+                }}
+            />
             )}
         </div>
     )
