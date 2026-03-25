ec38e866ae8fedc6768adc636c1771850eaed109
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-03-25 12:48:16 -0400

    Assigning the images group to the board.
    The user should choose an image group and that image group will be presented on the board.

--- client/src/Games/Puzzle/Puzzle.jsx
+++ client/src/Games/Puzzle/Puzzle.jsx
@@ -1,22 +1,53 @@
+import { useState } from "react";
 import Cell from "./Cell";
 import { bluePicsArray, redPicsArray, board } from "./imagesArray";
 
 export default function Puzzle() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [imageGroup, setImageGroup] = useState(null);
+  const [isImageGroupChosen, setIsImageGroupChosen] = useState(false);
+
+  const handleStart = () => {
+    setIsGameStarted(true);
+  };
+  const handleImageGroup = (e) => {
+    if (e.target.value === "Blue Numbers") {
+      setImageGroup(bluePicsArray);
+    } else if (e.target.value === "Red Numbers") {
+      setImageGroup(redPicsArray);
+    }
+    setIsImageGroupChosen(true);
+  };
   return (
     <div>
-      <div
-        style={{
-          position: "relative",
-          top: "15px",
-          display: "grid",
-          gridTemplateColumns: "repeat(5, auto)",
-          justifyContent: "center",
-        }}
-      >
-        {board.map((cell) => (
-          <Cell image={cell.image} />
-        ))}
-      </div>
+      {!isGameStarted && <button onClick={handleStart}>Start the Game</button>}
+      {isGameStarted &&
+      <div>
+        <label htmlFor="imageGroup"></label>
+        <select onChange={handleImageGroup} name="imageGroup" id="imageGroup">
+          <option value={imageGroup} disabled selected>
+            Select an image group
+          </option>
+          {["Blue Numbers", "Red Numbers"].map((c) => (
+            <option>{c}</option>
+          ))}
+        </select>
+      </div>}
+      {isGameStarted && isImageGroupChosen && (
+        <div
+          style={{
+            position: "relative",
+            top: "15px",
+            display: "grid",
+            gridTemplateColumns: "repeat(5, auto)",
+            justifyContent: "center",
+          }}
+        >
+          {board.map((cell, idx) => (
+            <Cell image={idx < 25 ? imageGroup[idx].image : cell.image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
