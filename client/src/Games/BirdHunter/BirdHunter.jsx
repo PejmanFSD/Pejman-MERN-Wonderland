a54853c896da90053e2d8049b9593764fba028fd
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-04-21 14:19:59 -0400

    creating multiple hunting grounds.
    There're 8 hunting grounds for the game.

--- client/src/Games/BirdHunter/BirdHunter.jsx
+++ client/src/Games/BirdHunter/BirdHunter.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import HuntingGround from "./HuntingGround";
+import {getRandArr} from "../utils";
 
 export default function BirdHunter() {
+    const [grounds, setGrounds] = useState([1,2,3,4,5,6,7,8])
+    const [chosenGround, setChosenGround] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const handleChooseGround = () => {
+        const c = getRandArr(grounds);
+        setChosenGround(c);
+        setGrounds(currGrounds => currGrounds.filter(g => g !== c));
+    }
     return (
         <div>
-            <HuntingGround />
+            {grounds.map((g) => <div style={{display: "inline"}}>{g}-</div>)}
+            <div>chosenGround: {chosenGround}</div>
+            <HuntingGround groundNum={1} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={2} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={3} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={4} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={5} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={6} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={7} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <HuntingGround groundNum={8} chosenGround={chosenGround} isRunning={isRunning} setIsRunning={setIsRunning} />
+            <button style={{position: "relative", top: "10px"}} onClick={handleChooseGround} disabled={isRunning}>
+                Start
+            </button>
         </div>
     )
 }
--- client/src/Games/BirdHunter/HuntingGround.jsx
+++ client/src/Games/BirdHunter/HuntingGround.jsx
@@ -1,43 +1,63 @@
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import DynamicImage from "./DynamicImage";
 import A from "./images/A.jpg";
 import B from "./images/B.jpg";
-import C from "./images/C.jpg";
 
-export default function HuntingGround() {
+export default function HuntingGround({
+  groundNum,
+  chosenGround,
+  isRunning,
+  setIsRunning,
+}) {
   const [images, setImages] = useState(Array(8).fill(A));
-  const [isRunning, setIsRunning] = useState(false);
+  //   const [isRunning, setIsRunning] = useState(false);
   const stopRef = useRef(false); // control flag
-  
+
   const delay = (ms) => new Promise((res) => setTimeout(res, ms));
   // setTimeout(res, ms) -> waits ms milliseconds, then calls res()
   // res is provided automatically by JavaScript when we create a Promise,
   // so when we create a Promise, JavaScript calls our function immediately
   // and passes in a function as the first argument.
-  const handleClick = async () => {
-    if (isRunning) return; // Preventing double clicks
-    setIsRunning(true);
-    for (let i = 0; i < images.length; i++) {
-        if (stopRef.current) break; // 👈 stop check
-            // Turn current image to B
-            setImages((currImages) => currImages.map((img, idx) => (idx === i ? B : img)));
-            await delay(1000);
-            if (stopRef.current) break; // 👈 stop check again
-            // Turn it back to A
-            setImages((currImages) => currImages.map((img, idx) => (idx === i ? A : img)));
-    }
-    setIsRunning(false);
-  };
+  //   const handleClick = async () => {
+  useEffect(() => {
+    const runningTheChosenGround = async () => {
+      if (chosenGround === groundNum) {
+        if (isRunning) return; // Preventing double clicks
+        setIsRunning(true);
+        for (let i = 0; i < images.length; i++) {
+          if (stopRef.current) break; // 👈 stop check
+          // Turn current image to B
+          setImages((currImages) =>
+            currImages.map((img, idx) => (idx === i ? B : img)),
+          );
+          await delay(1000);
+          if (stopRef.current) break; // 👈 stop check again
+          // Turn it back to A
+          setImages((currImages) =>
+            currImages.map((img, idx) => (idx === i ? A : img)),
+          );
+        }
+        setIsRunning(false);
+      }
+    };
+    runningTheChosenGround();
+  }, [chosenGround]);
 
   return (
     <div>
-      <button style={{position: "relative", top: "10px"}} onClick={handleClick} disabled={isRunning}>
+      {/* <button style={{position: "relative", top: "10px"}} onClick={handleClick} disabled={isRunning}>
         Start
-      </button>
+      </button> */}
 
       <div>
         {images.map((src, index) => (
-        <DynamicImage index={index} src={src} setImages={setImages} setIsRunning={setIsRunning} stopRef={stopRef} />
+          <DynamicImage
+            index={index}
+            src={src}
+            setImages={setImages}
+            setIsRunning={setIsRunning}
+            stopRef={stopRef}
+          />
         ))}
       </div>
     </div>
