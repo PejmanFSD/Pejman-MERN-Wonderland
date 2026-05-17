223e085ab3c89f1f49d7dba0079bb1711c36b185
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-05-17 14:43:08 -0400

    Implementing the "Easy" mode and the "Normal" mode.
    The user can choose to play in either "Easy" mode or "Normal" mode.

--- client/src/Games/BlackJack/BlackJack.jsx
+++ client/src/Games/BlackJack/BlackJack.jsx
@@ -1,8 +1,61 @@
-import {chips, clubs, diamonds, hearts, spades} from "./images";
-export default function BlackJack({updateTotalPoint, currentUser}) {
-    return (
+import { useState } from "react";
+import ReviewSection from "../../Components/ReviewSection";
+import ModeExplaination from "../ModeExplaination";
+import { deckArray, chips, clubs, diamonds, hearts, spades } from "./images";
+
+export default function BlackJack({ updateTotalPoint, currentUser }) {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [easyMode, setEasyMode] = useState(false);
+  const [normalMode, setNormalMode] = useState(false);
+  const [deck, setDeck] = useState(deckArray);
+  const [usedCards, setUsedCards] = useState([]);
+
+  const handleEasyMode = () => {
+    setEasyMode(true);
+    setNormalMode(false);
+  };
+  const handleNormalMode = () => {
+    setNormalMode(true);
+    setEasyMode(false);
+  };
+  const handleStart = () => {
+    setIsGameStarted(true);
+  };
+  return (
+    <div>
+      <h2>BlackJack</h2>
+      {easyMode && !normalMode ? (
+        // !isTogglingReset &&
+        // !isTogglingLevel &&
+        // !isTogglingHomePage &&
+        <ModeExplaination message="Easy Mode: Pejman doesn't memorize the cards and if both hands have the same value, you win the round. You get one star if you win." />
+      ) : (
+        !easyMode &&
+        normalMode && (
+          //   !isTogglingReset &&
+          //   !isTogglingLevel &&
+          //   !isTogglingHomePage &&
+          <ModeExplaination message="Normal Mode: Pejman memorizes the cards and if both hands have the same value, Pejman wins the round. You get three star if you win." />
+        )
+      )}
+      {!isGameStarted && !easyMode && !normalMode && (
         <div>
-            <h2>BlackJack</h2>
+          <button onClick={handleEasyMode}>Easy Mode</button>
+          <button onClick={handleNormalMode}>Normal Mode</button>
         </div>
-    )
+      )}
+      {!isGameStarted && (easyMode || normalMode) && (
+        // !isTogglingLevel &&
+        // !isTogglingHomePage &&
+        <button onClick={handleStart}>Start the Game</button>
+      )}
+      {/* {isGameStarted &&
+        !isTogglingReset &&
+        !isTogglingLevel &&
+        !isTogglingHomePage &&
+        (
+          <ReviewSection game="Snake" currentUser={currentUser} />
+        )} */}
+    </div>
+  );
 }
