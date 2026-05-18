10b3e55b418a2c04448bbd7d7ac9dc649f1edd8f
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-05-17 20:22:37 -0400

    Implementing the raise functionality.
    The user can raise the chips in their turn.

--- client/src/Games/BlackJack/BlackJack.jsx
+++ client/src/Games/BlackJack/BlackJack.jsx
@@ -19,6 +19,8 @@ export default function BlackJack({ updateTotalPoint, currentUser }) {
   const [bet, setBet] = useState(0);
   const [isBetMade, setIsBetMade] = useState(false);
   const [roundNum, setRoundNum] = useState(1);
+  const [isRaising, setIsRaising] = useState(false);
+  const [raise, setRaise] = useState(0);
 
   const handleEasyMode = () => {
     setEasyMode(true);
@@ -42,24 +44,37 @@ export default function BlackJack({ updateTotalPoint, currentUser }) {
     return arr;
   };
   const getNewCardForUser = () => {
-    setUserHand(currUserHand => [...currUserHand, deck[0]]);
-    setDeck(currDeck => currDeck.filter(c => currDeck.indexOf(c) !== 0));
+    setUserHand((currUserHand) => [...currUserHand, deck[0]]);
+    setUserPoint((currUserPoint) => currUserPoint + deck[0].point);
+    setDeck((currDeck) => currDeck.filter((c) => currDeck.indexOf(c) !== 0));
+    setUsedCards((currUsedCards) => [...currUsedCards, deck[0]]);
   };
   const handleBet = (e) => {
-    setBet(e.target.value);
+    setBet(Number(e.target.value));
   };
   const submitBet = (e) => {
     e.preventDefault();
     setIsBetMade(true);
-    setUserChipsNum(currUserChipsNum => currUserChipsNum - bet);
-    setPejmanChipsNum(currPejmanChipsNum => currPejmanChipsNum - bet);
+    setUserChipsNum((currUserChipsNum) => currUserChipsNum - bet);
+    setPejmanChipsNum((currPejmanChipsNum) => currPejmanChipsNum - bet);
   };
-  const handleRaise = () => {
-
+  const renderRaisingForm = () => {
+    setIsRaising(true);
   };
-  const handleStand = () => {
-
+  const handleRaise = (e) => {
+    setRaise(Number(e.target.value));
   };
+  const submitRaise = (e) => {
+    e.preventDefault();
+    setIsRaising(false);
+    setBet(currBet => currBet + raise);
+    setUserChipsNum((currUserChipsNum) => currUserChipsNum - raise);
+    setPejmanChipsNum((currPejmanChipsNum) => currPejmanChipsNum - raise);
+  };
+  const cancelRaising = () => {
+    setIsRaising(false);
+  }
+  const handleStand = () => {};
   return (
     <div>
       <h2>BlackJack</h2>
@@ -88,55 +103,124 @@ export default function BlackJack({ updateTotalPoint, currentUser }) {
         // !isTogglingHomePage &&
         <button onClick={handleStart}>Start the Game</button>
       )}
-      {isGameStarted && deck.map((c, i) => <img src={deck[i].imgSrc} height="45px" />)}
-      {isGameStarted && <h3>Round: {roundNum}</h3>}
-      {isGameStarted && isBetMade && <h4 style={{color: "red"}}>bet: {bet}</h4>}
       {isGameStarted &&
+        deck.map((c, i) => <img src={deck[i].imgSrc} height="45px" />)}{" "}
+      <br />
+      {isGameStarted &&
+        usedCards.map((c, i) => (
+          <img src={usedCards[i].imgSrc} height="45px" />
+        ))}
+      {isGameStarted && <h3>Round: {roundNum}</h3>}
+      {isGameStarted &&
+    //   isBetMade &&
+      (
+        <h4 style={{ color: "red" }}>bet: {bet}</h4>
+      )}
+      {isGameStarted &&
+      (
+        <h4 style={{ color: "red" }}>raise: {raise}</h4>
+      )}
+      {isGameStarted && userHand.length > 0 && (
+        <h4 style={{ color: "red" }}>User's Point: {userPoint}</h4>
+      )}
+      {isGameStarted && (
         <div>
-            {new Array(pejmanChipsNum).fill(null).map(c => <img src={chips[1]} height="50px" />)}
+          {new Array(pejmanChipsNum).fill(null).map((c) => (
+            <img src={chips[1]} height="50px" />
+          ))}
         </div>
-      }
-      {isGameStarted && isBetMade &&
+      )}
+      {isGameStarted && isBetMade && (
         <div>
-            {new Array(2 * bet).fill(null).map(c => <img src={chips[0]} height="50px" />)}
+          {new Array(2 * bet).fill(null).map((c) => (
+            <img src={chips[0]} height="50px" />
+          ))}
         </div>
-      }
-      {isGameStarted && isUserTurn && new Array(userHand.length).fill(null).map((c, i) => <img src={userHand[i].imgSrc} height="50px" />)}<br />
-      {isGameStarted && isUserTurn && userHand.length === 0 && <button onClick={getNewCardForUser}>Start the round</button>}
-      {isGameStarted && isUserTurn && userHand.length === 1 && !isBetMade &&
-        
-<form onSubmit={submitBet}>
+      )}
+      {isGameStarted &&
+        isUserTurn &&
+        new Array(userHand.length)
+          .fill(null)
+          .map((c, i) => (
+            <img
+              src={userHand[i].imgSrc}
+              height="120px"
+              style={{ margin: "2px" }}
+            />
+          ))}
+      <br />
+      {isGameStarted && isUserTurn && userHand.length === 0 && (
+        <button onClick={getNewCardForUser}>Start the round</button>
+      )}
+      {isGameStarted && isUserTurn && userHand.length === 1 && !isBetMade && (
+        <form onSubmit={submitBet}>
+          <div>
+            <label htmlFor="bet">Make your bets</label>
+            <select onChange={handleBet} name="bet" id="bet">
+              <option value={bet} disabled selected>
+                🔽
+              </option>
+              {Array.from({ length: userChipsNum }, (_, i) => i + 1).map(
+                (i) => (
+                  <option>{i}</option>
+                ),
+              )}
+            </select>
+          </div>
+          {bet > 0 && <button>Submit your bet</button>}
+        </form>
+      )}
+      {isGameStarted &&
+        isUserTurn &&
+        userHand.length >= 1 &&
+        isBetMade &&
+        !isRaising && <button onClick={getNewCardForUser}>Hit</button>}
+      {isGameStarted &&
+        isUserTurn &&
+        userHand.length >= 1 &&
+        isBetMade &&
+        !isRaising && (
+          <button onClick={renderRaisingForm} disabled={userHand.length === 1}>
+            Raise
+          </button>
+        )}
+      {isGameStarted &&
+        isUserTurn &&
+        userHand.length >= 1 &&
+        isBetMade &&
+        !isRaising && (
+          <button onClick={handleStand} disabled={userHand.length === 1}>
+            Stand
+          </button>
+        )}
+      {isRaising && (
+        <div>
+          <form onSubmit={submitRaise}>
             <div>
-              <label htmlFor="bet">Make your bets</label>
-              <select
-                onChange={handleBet}
-                name="bet"
-                id="bet"
-              >
-                <option value={bet} disabled selected>
+              <label htmlFor="raise">How much do you want to raise?</label>
+              <select onChange={handleRaise} name="raise" id="raise">
+                <option value={raise} disabled selected>
                   🔽
                 </option>
-                {Array.from({ length: userChipsNum }, (_, i) => i + 1).map((i) => (
-                  <option>{i}</option>
-                ))}
+                {Array.from({ length: userChipsNum }, (_, i) => i + 1).map(
+                  (i) => (
+                    <option>{i}</option>
+                  ),
+                )}
               </select>
             </div>
-            <button>Submit your bet</button>
+            <button disabled={raise === 0}>Update the raise</button>
           </form>
-
-
-
-
-
-      }
-      {isGameStarted && isUserTurn && userHand.length >= 1 && isBetMade && <button onClick={getNewCardForUser}>Hit</button>}
-      {isGameStarted && isUserTurn && userHand.length >= 1 && isBetMade && <button onClick={handleRaise}>Raise</button>}
-      {isGameStarted && isUserTurn && userHand.length >= 1 && isBetMade && <button onClick={handleStand}>Stand</button>}
-      {isGameStarted &&
-        <div>
-            {new Array(userChipsNum).fill(null).map(c => <img src={chips[1]} height="50px" />)}
+          <button onClick={cancelRaising}>Cancel the raise</button>
         </div>
-      }
+      )}
+      {isGameStarted && (
+        <div>
+          {new Array(userChipsNum).fill(null).map((c) => (
+            <img src={chips[1]} height="50px" />
+          ))}
+        </div>
+      )}
       {/* {isGameStarted &&
         !isTogglingReset &&
         !isTogglingLevel &&
