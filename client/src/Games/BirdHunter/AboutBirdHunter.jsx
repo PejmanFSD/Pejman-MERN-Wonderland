3125e70d5f4271ab26f8e723466da282d5474b75
Pejmanfsd <180977377+PejmanFSD@users.noreply.github.com>
2026-06-01 11:37:47 -0400

    Implementing the "About Bird Hunter" page.
    The user can read the instructions of the "Bird Hunter" game.

---
+++ client/src/Games/BirdHunter/AboutBirdHunter.jsx
@@ -0,0 +1,40 @@
+export default function AboutBirdHunter({ setIsAboutPage }) {
+  const handleGamePage = () => {
+    setIsAboutPage(false);
+  };
+  return (
+    <div>
+      <h2>About Bird Hunter</h2>
+      In this game, there are 16 different landscapes, each containing one tree
+      on the left and one tree on the right.
+      <br />
+      In every landscape, a bird flies from the left tree toward the right tree,
+      and your goal is to shoot the bird by clicking on it before it reaches its
+      destination.
+      <br />
+      <h4>How the Game Works</h4>
+      Only one bird appears on the screen at a time. A new bird begins flying
+      only after:
+      <br />
+      The previous bird has been shot, or
+      <br />
+      The previous bird successfully reaches the right tree
+      <br />
+      If a bird reaches the right tree before you click on it, you miss that
+      bird and it can no longer be shot.
+      <br />
+      The landscapes appear in a random order, not sequentially and each
+      landscape contains only one bird flight during the entire game.
+      <br />
+      As the game progresses, each new bird flies slightly faster than the
+      previous one so the challenge becomes more difficult over time.
+      <h4>Winning the Game</h4>
+      The game ends after all 16 landscapes have featured a bird flight and to
+      win you must successfully shoot at least 14 birds.
+      <br />
+      Win the game to earn 1 star.
+      <br />
+      <button onClick={handleGamePage}>Back to the Game</button>
+    </div>
+  );
+}
--- client/src/Games/BirdHunter/BirdHunter.jsx
+++ client/src/Games/BirdHunter/BirdHunter.jsx
@@ -6,8 +6,10 @@ import T2 from "./images/T2.jpg";
 import ConfirmationBox from "../ConfirmationBox";
 import { useNavigate } from "react-router-dom";
 import ReviewSection from "../../Components/ReviewSection";
+import AboutBirdHunter from "./AboutBirdHunter";
 
-export default function BirdHunter({updateTotalPoint, currentUser}) {
+export default function BirdHunter({ updateTotalPoint, currentUser }) {
+  const [isAboutPage, setIsAboutPage] = useState(false);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [grounds, setGrounds] = useState([
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
@@ -58,120 +60,174 @@ export default function BirdHunter({updateTotalPoint, currentUser}) {
   const toggleHomePageCancel = () => {
     setIsTogglingHomePage(false);
   };
+  const handleAboutPage = () => {
+    setIsAboutPage(true);
+  };
   return (
     <div>
-      <h2>Bird Hunter</h2>
-      {grounds.map((g) => (
+      {isAboutPage && <AboutBirdHunter setIsAboutPage={setIsAboutPage} />}
+      {!isAboutPage && (
+        <div>
+          {!isTogglingHomePage && (
+            <button onClick={handleAboutPage}>About Bird Hunter</button>
+          )}
+          <h2>Bird Hunter</h2>
+          {/* {grounds.map((g) => (
         <div style={{ display: "inline" }}>{g}-</div>
-      ))}
-      <div>chosenGround: {chosenGround}</div>
-      <div>numOfDoneGrounds: {numOfDoneGrounds}</div>
-      <div>userScore: {userScore}</div>
-      {isGameStarted && (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <div style={{ display: "flex", gap: "50px" }}>
-            <div style={{ display: "inline" }}>
-              {[1, 2, 3, 4, 5, 6, 7, 8].map((el) => (
-                <div style={{ display: "flex", alignItems: "center", border: "1px solid black", margin: "5px", padding: "7px" }}>
-                    <img src={T1} height={isTogglingHomePage ? "30px" : "55px"} />
-                    <HuntingGround
-                    grounds={grounds}
-                    groundNum={el}
-                    chosenGround={chosenGround}
-                    isRunning={isRunning}
-                    setIsRunning={setIsRunning}
-                    delayMilliSec={delayMilliSec}
-                    setDelayMilliSec={setDelayMilliSec}
-                    handleChooseGround={handleChooseGround}
-                    setChosenGround={setChosenGround}
-                    setNumOfDoneGrounds={setNumOfDoneGrounds}
-                    setUserScore={setUserScore}
-                    isTogglingHomePage={isTogglingHomePage}
-                    />
-                    <img src={T2} height={isTogglingHomePage ? "30px" : "55px"} />
-                </div>
-              ))}
+      ))} */}
+          {/* <div>chosenGround: {chosenGround}</div> */}
+          {/* <div>numOfDoneGrounds: {numOfDoneGrounds}</div> */}
+          {/* <div>userScore: {userScore}</div> */}
+          {!isTogglingHomePage && (
+            <div>
+              <button onClick={() => toggleHomePage()}>
+                Back to the home page
+              </button>
             </div>
-            <div style={{ display: "inline" }}>
-              {[9, 10, 11, 12, 13, 14, 15, 16].map((el) => (
-                <div style={{ display: "flex", alignItems: "center", border: "1px solid black", margin: "5px", padding: "7px" }}>
-                    <img src={T1} height={isTogglingHomePage ? "30px" : "55px"} />
-                    <HuntingGround
-                    grounds={grounds}
-                    groundNum={el}
-                    chosenGround={chosenGround}
-                    isRunning={isRunning}
-                    setIsRunning={setIsRunning}
-                    delayMilliSec={delayMilliSec}
-                    setDelayMilliSec={setDelayMilliSec}
-                    handleChooseGround={handleChooseGround}
-                    setChosenGround={setChosenGround}
-                    setNumOfDoneGrounds={setNumOfDoneGrounds}
-                    setUserScore={setUserScore}
-                    isTogglingHomePage={isTogglingHomePage}
-                    />
-                    <img src={T2} height={isTogglingHomePage ? "30px" : "55px"} />
-                </div>
-              ))}
+          )}
+          {isTogglingHomePage && (
+            <div>
+              <ConfirmationBox
+                question="Are you sure you want to go back to Home Page?"
+                toggleYes={toggleHomePageYes}
+                toggleCancel={toggleHomePageCancel}
+              />
             </div>
+          )}
+          {isGameStarted && (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{ display: "flex", gap: "50px" }}>
+                <div style={{ display: "inline" }}>
+                  {[1, 2, 3, 4, 5, 6, 7, 8].map((el) => (
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        border: "1px solid black",
+                        margin: "5px",
+                        padding: "7px",
+                      }}
+                    >
+                      <img
+                        src={T1}
+                        height={isTogglingHomePage ? "30px" : "55px"}
+                      />
+                      <HuntingGround
+                        grounds={grounds}
+                        groundNum={el}
+                        chosenGround={chosenGround}
+                        isRunning={isRunning}
+                        setIsRunning={setIsRunning}
+                        delayMilliSec={delayMilliSec}
+                        setDelayMilliSec={setDelayMilliSec}
+                        handleChooseGround={handleChooseGround}
+                        setChosenGround={setChosenGround}
+                        setNumOfDoneGrounds={setNumOfDoneGrounds}
+                        setUserScore={setUserScore}
+                        isTogglingHomePage={isTogglingHomePage}
+                      />
+                      <img
+                        src={T2}
+                        height={isTogglingHomePage ? "30px" : "55px"}
+                      />
+                    </div>
+                  ))}
+                </div>
+                <div style={{ display: "inline" }}>
+                  {[9, 10, 11, 12, 13, 14, 15, 16].map((el) => (
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        border: "1px solid black",
+                        margin: "5px",
+                        padding: "7px",
+                      }}
+                    >
+                      <img
+                        src={T1}
+                        height={isTogglingHomePage ? "30px" : "55px"}
+                      />
+                      <HuntingGround
+                        grounds={grounds}
+                        groundNum={el}
+                        chosenGround={chosenGround}
+                        isRunning={isRunning}
+                        setIsRunning={setIsRunning}
+                        delayMilliSec={delayMilliSec}
+                        setDelayMilliSec={setDelayMilliSec}
+                        handleChooseGround={handleChooseGround}
+                        setChosenGround={setChosenGround}
+                        setNumOfDoneGrounds={setNumOfDoneGrounds}
+                        setUserScore={setUserScore}
+                        isTogglingHomePage={isTogglingHomePage}
+                      />
+                      <img
+                        src={T2}
+                        height={isTogglingHomePage ? "30px" : "55px"}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+          {grounds.length === 16 && !isTogglingHomePage && (
+            <button
+              style={{ position: "relative", top: "5px" }}
+              onClick={handleStart}
+              disabled={isRunning}
+            >
+              Start the Game
+            </button>
+          )}
+          {numOfDoneGrounds === 16 &&
+            finalMessage === "" &&
+            !isTogglingHomePage && (
+              <button
+                style={{ position: "relative", top: "5px" }}
+                onClick={handleAnnouncingTheGameResult}
+              >
+                See the Game Result
+              </button>
+            )}
+          <h2>{finalMessage && !isTogglingHomePage && finalMessage}</h2>
+          <div>
+            {finalMessage &&
+              userScore === 16 &&
+              !isTogglingHomePage &&
+              "Wow! You didn't miss a single bird!"}
           </div>
+          <div>
+            {finalMessage &&
+              userScore === 15 &&
+              !isTogglingHomePage &&
+              "You missed a bird!"}
+          </div>
+          <div>
+            {finalMessage &&
+              userScore < 15 &&
+              !isTogglingHomePage &&
+              `You missed ${16 - userScore} birds!`}
+          </div>
+          {finalMessage === "You Win!" && !isTogglingHomePage && (
+            <div>
+              <div>Play Again!</div>
+              <button onClick={handlePlayAgain}>Ok</button>
+            </div>
+          )}
+          {finalMessage === "You Loose!" && !isTogglingHomePage && (
+            <div>
+              <div>Try Again!</div>
+              <button onClick={handlePlayAgain}>Ok</button>
+            </div>
+          )}
+
+          {!isTogglingHomePage && isGameStarted && (
+            <ReviewSection game="BirdHunter" currentUser={currentUser} />
+          )}
         </div>
       )}
-      {grounds.length === 16 && !isTogglingHomePage && (
-        <button
-          style={{ position: "relative", top: "5px" }}
-          onClick={handleStart}
-          disabled={isRunning}
-        >
-          Start the Game
-        </button>
-      )}
-      {numOfDoneGrounds === 16 && finalMessage === "" && !isTogglingHomePage && (
-        <button
-          style={{ position: "relative", top: "5px" }}
-          onClick={handleAnnouncingTheGameResult}
-        >
-          See the Game Result
-        </button>
-      )}
-      <h2>{finalMessage && !isTogglingHomePage && finalMessage}</h2>
-      <div>
-        {finalMessage && userScore === 16 && !isTogglingHomePage && "Wow! You didn't miss a single bird!"}
-      </div>
-      <div>{finalMessage && userScore === 15 && !isTogglingHomePage && "You missed a bird!"}</div>
-      <div>
-        {finalMessage && userScore < 15 && !isTogglingHomePage &&
-          `You missed ${16 - userScore} birds!`}
-      </div>
-      {finalMessage === "You Win!" && !isTogglingHomePage && (
-        <div>
-          <div>Play Again!</div>
-          <button onClick={handlePlayAgain}>Ok</button>
-        </div>
-      )}
-      {finalMessage === "You Loose!" && !isTogglingHomePage && (
-        <div>
-          <div>Try Again!</div>
-          <button onClick={handlePlayAgain}>Ok</button>
-        </div>
-      )}
-      {!isTogglingHomePage && (
-        <div>
-          <button onClick={() => toggleHomePage()}>
-            Back to the home page
-          </button>
-        </div>
-      )}
-      {isTogglingHomePage && (
-        <div>
-          <ConfirmationBox
-            question="Are you sure you want to go back to Home Page?"
-            toggleYes={toggleHomePageYes}
-            toggleCancel={toggleHomePageCancel}
-          />
-        </div>
-      )}
-      {!isTogglingHomePage && isGameStarted && <ReviewSection game="BirdHunter" currentUser={currentUser} />}
     </div>
   );
 }
